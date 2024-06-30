9f4c3dbc9b8d5ad17585c76032b53e924c655897
Choyoungjun <cyj070513@gmail.com>
2024-06-30 21:41:28 +0900

    ADD :: OnBoardingViewController 추가

---
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingViewController.swift
+//  Presentation
+//
+//  Created by 조영준 on 6/30/24.
+//  Copyright © 2024 com.pick-v2.app. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
