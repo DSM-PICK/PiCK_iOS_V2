d031d80d9d3fa42d672ff2233f02d75282208f39
Choyoungjun <cyj070513@gmail.com>
2024-07-01 21:54:27 +0900

    ADD :: Onboarding

--- Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewController.swift
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewController.swift
@@ -1,30 +1,18 @@
-//
-//  OnBoardingViewController.swift
-//  Presentation
-//
-//  Created by 조영준 on 6/30/24.
-//  Copyright © 2024 com.pick-v2.app. All rights reserved.
-//
-
 import UIKit
 
-class OnBoardingViewController: UIViewController {
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import Lottie
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+import Core
+import DesignSystem
 
-        // Do any additional setup after loading the view.
+public class OnboardingViewController: BaseViewController<OnboardingViewModel> {
+
+    public override func attribute() {
+        super.attribute()
+        self.view.backgroundColor = .red
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
