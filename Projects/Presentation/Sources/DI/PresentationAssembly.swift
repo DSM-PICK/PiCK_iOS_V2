73f9809ef5214da9102814e6cb678e769b548b70
Choyoungjun <cyj070513@gmail.com>
2024-07-01 21:55:37 +0900

    REFACT :: 주석 해제

--- Projects/Presentation/Sources/DI/PresentationAssembly.swift
+++ Projects/Presentation/Sources/DI/PresentationAssembly.swift
@@ -9,15 +9,15 @@ public final class PresentationAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
-        //onboarding
-//        container.register(OnboardingViewController.self) { resolver in
-//            OnboardingViewController(viewModel: resolver.resolve(OnboardingViewModel.self)!)
-//        }
-//        container.register(OnboardingViewModel.self) { resolver in
-//            OnboardingViewModel()
-//        }
-//        
-//        //login
+//        onboarding
+        container.register(OnboardingViewController.self) { resolver in
+            OnboardingViewController(viewModel: resolver.resolve(OnboardingViewModel.self)!)
+        }
+        container.register(OnboardingViewModel.self) { resolver in
+            OnboardingViewModel()
+        }
+        
+        //login
 //        container.register(LoginViewController.self) { resolver in
 //            LoginViewController(viewModel: resolver.resolve(LoginViewModel.self)!)
 //        }
