cbd8ffbce4203c0b684b834788b7fc897cdac51a
Choyoungjun <cyj070513@gmail.com>
2024-07-02 13:37:40 +0900

    REFACT :: extension

--- Projects/Modules/DesignSystem/Sources/Extension/Color/UIColor+.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/Color/UIColor+.swift
@@ -1,26 +1,26 @@
 import UIKit
 
 public extension UIColor {
-    static let background = DesignSystemAsset.background.color
-    static let error = DesignSystemAsset.error.color
-    static let gray50 = DesignSystemAsset.gray50.color
-    static let gray100 = DesignSystemAsset.gray100.color
-    static let gray200 = DesignSystemAsset.gray200.color
-    static let gray300 = DesignSystemAsset.gray300.color
-    static let gray400 = DesignSystemAsset.gray400.color
-    static let gray500 = DesignSystemAsset.gray500.color
-    static let gray600 = DesignSystemAsset.gray600.color
-    static let gray700 = DesignSystemAsset.gray700.color
-    static let gray800 = DesignSystemAsset.gray800.color
-    static let gray900 = DesignSystemAsset.gray900.color
-    static let main50 = DesignSystemAsset.main50.color
-    static let main100 = DesignSystemAsset.main100.color
-    static let main200 = DesignSystemAsset.main200.color
-    static let main300 = DesignSystemAsset.main300.color
-    static let main400 = DesignSystemAsset.main400.color
-    static let main500 = DesignSystemAsset.main500.color
-    static let main600 = DesignSystemAsset.main600.color
-    static let main700 = DesignSystemAsset.main700.color
-    static let main800 = DesignSystemAsset.main800.color
-    static let main900 = DesignSystemAsset.main900.color
+    static let background = DesignSystemAsset.Color.background.color
+    static let error = DesignSystemAsset.Color.error.color
+    static let gray50 = DesignSystemAsset.Color.gray50.color
+    static let gray100 = DesignSystemAsset.Color.gray100.color
+    static let gray200 = DesignSystemAsset.Color.gray200.color
+    static let gray300 = DesignSystemAsset.Color.gray300.color
+    static let gray400 = DesignSystemAsset.Color.gray400.color
+    static let gray500 = DesignSystemAsset.Color.gray500.color
+    static let gray600 = DesignSystemAsset.Color.gray600.color
+    static let gray700 = DesignSystemAsset.Color.gray700.color
+    static let gray800 = DesignSystemAsset.Color.gray800.color
+    static let gray900 = DesignSystemAsset.Color.gray900.color
+    static let main50 = DesignSystemAsset.Color.main50.color
+    static let main100 = DesignSystemAsset.Color.main100.color
+    static let main200 = DesignSystemAsset.Color.main200.color
+    static let main300 = DesignSystemAsset.Color.main300.color
+    static let main400 = DesignSystemAsset.Color.main400.color
+    static let main500 = DesignSystemAsset.Color.main500.color
+    static let main600 = DesignSystemAsset.Color.main600.color
+    static let main700 = DesignSystemAsset.Color.main700.color
+    static let main800 = DesignSystemAsset.Color.main800.color
+    static let main900 = DesignSystemAsset.Color.main900.color
 }
