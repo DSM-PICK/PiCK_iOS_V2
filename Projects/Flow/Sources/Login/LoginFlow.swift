a6e0dda717f90fd7396da563a68aa6fd2d19926d
Choyoungjun <cyj070513@gmail.com>
2024-07-02 13:06:25 +0900

    ADD :: LoginFlow

---
+++ Projects/Flow/Sources/Login/LoginFlow.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+import RxFlow
+import Swinject
+
+import Core
+import Presentation
+
+public class LoginFlow: Flow {
+    public let container: Container
+    private let rootViewController: LoginViewController
+    public var root: Presentable {
+        return rootViewController
+    }
+    
+    public init(container: Container) {
+        self.container = container
+        self.rootViewController = container.resolve(LoginViewController.self)!
+    }
+
+    public func navigate(to step: RxFlow.Step) -> RxFlow.FlowContributors {
+        guard let step = step as? PiCKStep else { return .none }
+        
+        switch step {
+            case .loginIsRequired:
+                return navigateToLogin()
+//            case .mainRequired:
+//                return navigateToMain()
+            default:
+                return .none
+        }
+    }
+    
+    private func navigateToLogin() -> FlowContributors {
+        let viewModel = container.resolve(LoginViewModel.self)!
+        let vc = LoginViewController(
+            viewModel: viewModel
+        )
+        self.rootViewController.navigationItem.hidesBackButton = true
+        return .one(flowContributor: .contribute(
+            withNextPresentable: vc,
+            withNextStepper: vc.viewModel
+        ))
+    }
+    
+//    private func navigateToMain() -> FlowContributors {
+//        return .end(forwardToParentFlowWithStep: PiCKStep.mainRequired)
+//    }
+
+}
