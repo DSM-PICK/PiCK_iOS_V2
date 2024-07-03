250649b96c879fe2ac9f5c10c26f7848ec2b6f46
Choyoungjun <cyj070513@gmail.com>
2024-07-03 14:27:45 +0900

    ADD :: Image

---
+++ Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public extension UIImage {
+    //MARK: Icon
+    static let eyeOn = DesignSystemAsset.Image.eyeOn.image
+    static let eyeOff = DesignSystemAsset.Image.eyeOff.image
+}
