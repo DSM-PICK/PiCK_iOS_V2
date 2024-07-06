026f6e38f90fa5832fec1a26fe61badee9dc2293
Choyoungjun <cyj070513@gmail.com>
2024-07-07 04:28:30 +0900

    ADD :: TestFlow

---
+++ Projects/Flow/Sources/Test/TestFlow.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+import RxFlow
+import Swinject
+
+import Core
+import Presentation
+
+public class TestFlow: Flow {
+    public let container: Container
+    private let rootViewController = BaseNavigationController()
+    public var root: Presentable {
+        return rootViewController
+    }
+    
+    public init(container: Container) {
+        self.container = container
+    }
+    
+    public func navigate(to step: RxFlow.Step) -> RxFlow.FlowContributors {
+        guard let step = step as? PiCKStep else { return .none }
+        
+        switch step {
+        case .testIsRequired:
+            return navigateToTest()
+        default:
+            return .none
+        }
+    }
+    
+    private func navigateToTest() -> FlowContributors {
+        let viewModel = MainViewModel()
+        let vc = MainViewController(
+            viewModel: viewModel
+        )
+        self.rootViewController.pushViewController(vc, animated: true)
+        return .one(flowContributor: .contribute(
+            withNextPresentable: vc,
+            withNextStepper: vc.viewModel
+        ))
+    }
+    
+}
