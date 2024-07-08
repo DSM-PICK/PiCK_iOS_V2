a89e8908df42152fe0e7d1bf97f4e7c572b122bf
Choyoungjun <cyj070513@gmail.com>
2024-07-08 15:06:12 +0900

    REFACT :: main -> home

--- Projects/Presentation/Sources/Scene/Login/LoginViewModel.swift
+++ Projects/Presentation/Sources/Scene/Login/LoginViewModel.swift
@@ -21,7 +21,7 @@ public class LoginViewModel: BaseViewModel, Stepper {
     
     public func transform(input: Input) -> Output {
         input.loginButtonClick
-            .map { PiCKStep.mainIsRequired }
+            .map { PiCKStep.homeIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
         return Output()
