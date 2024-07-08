9aa3a8b96f74f478e643eadd90e89352d7c8a553
Choyoungjun <cyj070513@gmail.com>
2024-07-08 14:50:01 +0900

    ADD :: UIImage Extension

--- Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
@@ -1,7 +1,23 @@
 import UIKit
 
 public extension UIImage {
+    //MARK: Logo
+    static let PiCKLogo = DesignSystemAsset.Image.pickLogo.image
     //MARK: Icon
+    static let navigationSetting = DesignSystemAsset.Image.navigationSetting.image
+    static let displayMode = DesignSystemAsset.Image.displayMode.image
+    static let alert = DesignSystemAsset.Image.alert.image
+    static let profile = DesignSystemAsset.Image.profile.image
+    static let newNotice = DesignSystemAsset.Image.newNotice.image
+    static let notice = DesignSystemAsset.Image.newNotice.image
     static let eyeOn = DesignSystemAsset.Image.eyeOn.image
     static let eyeOff = DesignSystemAsset.Image.eyeOff.image
+    //MARK: TabBarIcon
+    static let homeIcon = DesignSystemAsset.Image.homeIcon.image
+    static let schoolMealIcon = DesignSystemAsset.Image.schoolMealIcon.image
+    static let applyIcon = DesignSystemAsset.Image.applyIcon.image
+    static let scheduleIcon = DesignSystemAsset.Image.scheduleIcon.image
+    static let allTabIcon = DesignSystemAsset.Image.allTabIcon.image
+    //MARK: Image
+    static let banner = DesignSystemAsset.Image.banner.image
 }
