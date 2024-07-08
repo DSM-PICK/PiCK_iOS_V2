ae6954470a7a670413bdc4dde37330c40861545e
Choyoungjun <cyj070513@gmail.com>
2024-07-08 14:49:27 +0900

    ADD :: Schedule

---
+++ Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class ScheduleViewController: BaseViewController<ScheduleViewModel> {
+
+    public override func attribute() {
+        super.attribute()
+        
+        view.backgroundColor = .green
+    }
+
+}
