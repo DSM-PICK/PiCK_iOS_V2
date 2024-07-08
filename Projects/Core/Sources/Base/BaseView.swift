5c73705045f3ff38844a3df4ea76e863270d5bf1
Choyoungjun <cyj070513@gmail.com>
2024-07-08 14:52:42 +0900

    ADD :: BaseView

---
+++ Projects/Core/Sources/Base/BaseView.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+open class BaseView: UIView {
+    public let disposeBag = DisposeBag()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+        bind()
+    }
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open func attribute() {
+        //뷰 관련 코드를 설정하는 함수
+    }
+    open func bind() {
+        //UI 바인딩을 설정하는 함수
+    }
+    open func layout() {
+        //레이아웃을 설정하는 함수
+    }
+}
