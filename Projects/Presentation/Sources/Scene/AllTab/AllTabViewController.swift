b8ea0045fa236c39929cb17b5e635a0b1682d4e2
Choyoungjun <cyj070513@gmail.com>
2024-07-08 14:49:43 +0900

    ADD :: AllTab

---
+++ Projects/Presentation/Sources/Scene/AllTab/AllTabViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class AllTabViewController: BaseViewController<AllTabViewModel> {
+
+    public override func attribute() {
+        super.attribute()
+        
+        view.backgroundColor = .red
+    }
+
+}
