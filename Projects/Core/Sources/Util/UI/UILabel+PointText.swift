805ced404528df3dd3a3dc7192558cd6b9c56b9a
Choyoungjun <cyj070513@gmail.com>
2024-07-09 20:00:45 +0900

    ADD :: PointFont

--- Projects/Core/Sources/Util/UI/UILabel+PointText.swift
+++ Projects/Core/Sources/Util/UI/UILabel+PointText.swift
@@ -8,4 +8,12 @@ public extension UILabel {
         attributedString.addAttribute(.foregroundColor, value: color, range: range)
         attributedText = attributedString
     }
+    
+    func changePointFont(targetString: String, font: UIFont) {
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttribute(.font, value: font, range: range)
+        attributedText = attributedString
+    }
 }
