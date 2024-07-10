d0c2aacff6355169c1bd3aa77d77381411d85a3e
Choyoungjun <cyj070513@gmail.com>
2024-07-10 15:15:22 +0900

    ADD :: BaseCollectionViewCell

---
+++ Projects/Core/Sources/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+open class BaseCollectionViewCell: UICollectionViewCell {
+    
+    private let disposeBag = DisposeBag()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        bindAction()
+    }
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        layout()
+    }
+    
+    open func bindAction() {
+        //cell 관련 액션을 설정하는 함수
+    }
+    open func attribute() {
+        //cell 관련 설정을 하는 함수
+    }
+    open func layout() {
+        //cell에 서브뷰를 추가하는 함수
+    }
+    
+}
