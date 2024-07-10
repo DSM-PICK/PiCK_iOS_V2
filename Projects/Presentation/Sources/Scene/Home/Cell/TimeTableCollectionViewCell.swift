05b27576cd11aa018ddeebb8ae69e5a3e007d480
Choyoungjun <cyj070513@gmail.com>
2024-07-10 15:18:01 +0900

    ADD :: Home Cell

---
+++ Projects/Presentation/Sources/Scene/Home/Cell/TimeTableCollectionViewCell.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import Core
+import DesignSystem
+
+public class TimeTableCollectionViewCell: BaseCollectionViewCell {
+    
+    static let identifier = "TimeTableCollectionViewCell"
+    
+    private let periodLabel = UILabel().then {
+        $0.textColor = .modeBlack
+        $0.font = .subTitle2
+    }
+    private let subjectImageView = UIImageView()
+    private let subjectLabel = UILabel().then {
+        $0.textColor = .modeBlack
+        $0.font = .label1
+    }
+    
+    public func setup(
+        period: String,
+        image: UIImage,
+        subject: String
+    ) {
+        self.periodLabel.text = "\(period)교시"
+        self.periodLabel.changePointColor(targetString: period, color: .main500)
+        self.subjectImageView.image = image
+        self.subjectLabel.text = subject
+    }
+    
+    public override func layout() {
+        [
+            periodLabel,
+            subjectImageView,
+            subjectLabel
+        ].forEach { self.addSubview($0) }
+        
+        periodLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(24)
+        }
+        subjectImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(periodLabel.snp.trailing).offset(32)
+        }
+        subjectLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(subjectImageView.snp.trailing).offset(12)
+        }
+    }
+
+}
