3a452b44ba58b676869b0fc25249afdd63a3f039
Choyoungjun <cyj070513@gmail.com>
2024-07-16 14:14:14 +0900

    ADD :: HelpSectionView

---
+++ Projects/Presentation/Sources/Scene/AllTab/Component/HelpSectionView.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+class HelpSectionView: BaseView {
+    
+    enum HelpSectionType: Int {
+        case announcement
+    }
+    
+    private let helpSectionView = PiCKSectionView(
+        menuText: "도움말",
+        items: [
+            ("공지사항", .voice),
+            ("자습 감독 선생님 확인", .smile),
+            ("버그 제보", .bug)
+        ]
+    )
+
+    override func layout() {
+        self.addSubview(helpSectionView)
+
+        helpSectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    func getSelectedItem(type: HelpSectionType) -> Observable<IndexPath> {
+        self.helpSectionView.getSelectedItem(index: type.rawValue)
+    }
+
+}
