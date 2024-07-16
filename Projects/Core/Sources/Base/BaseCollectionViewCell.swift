333578003f2161b7b32e728ffb9eed45449ecabb
Choyoungjun <cyj070513@gmail.com>
2024-07-16 14:08:53 +0900

    REFACT :: 함수 정리

--- Projects/Core/Sources/Base/BaseCollectionViewCell.swift
+++ Projects/Core/Sources/Base/BaseCollectionViewCell.swift
@@ -20,11 +20,11 @@ open class BaseCollectionViewCell: UICollectionViewCell {
         layout()
     }
     
-    open func bindAction() {
-        //cell 관련 액션을 설정하는 함수
-    }
     open func attribute() {
         //cell 관련 설정을 하는 함수
+    }
+    open func bindAction() {
+        //cell 관련 액션을 설정하는 함수
     }
     open func layout() {
         //cell에 서브뷰를 추가하는 함수
