a19a64fac9ae1bc1311a118eadf489e7f0cac20d
Choyoungjun <cyj070513@gmail.com>
2024-07-16 14:06:22 +0900

    ADD :: Data 로직

---
+++ Projects/Data/Sources/Outing/DTO/OutingPassDTO.swift
@@ -0,0 +1,38 @@
+//import Foundation
+//
+//import Core
+//import Domain
+//
+//public struct OutingPassDTO: Decodable {
+//    let userName: String
+//    let teacherName: String
+//    let startTime: String
+//    let endTime: String
+//    let reason: String
+//    let schoolNum: Int
+//    let type: OutingPassType.RawValue
+//    
+//    enum CodingKeys: String, CodingKey {
+//        case reason, type
+//        case userName = "username"
+//        case teacherName = "teacher_name"
+//        case startTime = "start_time"
+//        case endTime = "end_time"
+//        case schoolNum = "school_num"
+//    }
+//}
+//
+//extension OutingPassDTO {
+//    func toDomain() -> OutingPassEntity {
+//        return .init(
+//            userName: userName,
+//            teacherName: teacherName,
+//            startTime: startTime,
+//            endTime: endTime,
+//            reason: reason,
+//            schoolNum: schoolNum,
+//            type: type
+//        )
+//    }
+//    
+//}
---
+++ Projects/Data/Sources/Outing/DataSource/OutingDataSource.swift
@@ -0,0 +1,40 @@
+//import Foundation
+//
+//import RxSwift
+//import Moya
+//import RxMoya
+//
+//import AppNetwork
+//import Domain
+//
+//class OutingDataSource {
+//    private let provider = MoyaProvider<OutingAPI>(plugins: [MoyaLoggingPlugin()])
+//    
+//    static let shared = OutingDataSource()
+//    private init() {}
+//    
+//    func outingApply(reason: String, startTime: String, endTime: String) -> Completable {
+//        return provider.rx.request(.outingApply(
+//            reason: reason,
+//            startTime: startTime,
+//            endTime: endTime
+//        ))
+//        .asCompletable()
+//    }
+//    func fetchOutingPass() -> Single<Response> {
+//        return provider.rx.request(.fetchOutingPass)
+//            .filterSuccessfulStatusCodes()
+//    }
+//    func earlyLeaveApply(reason: String, startTime: String) -> Completable {
+//        return provider.rx.request(.earlyLeaveApply(
+//            reason: reason,
+//            startTime: startTime
+//        ))
+//        .asCompletable()
+//    }
+//    func fetchEarlyLeavePass() -> Single<Response> {
+//        return provider.rx.request(.fetchEarlyLeavePass)
+//            .filterSuccessfulStatusCodes()
+//    }
+//    
+//}
