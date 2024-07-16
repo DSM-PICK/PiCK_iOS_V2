00c9c5b87c0c3f874feeb3614af65600417a831d
Choyoungjun <cyj070513@gmail.com>
2024-07-16 14:16:43 +0900

    FEAT :: 전체 탭 뷰 퍼블리싱

--- Projects/Presentation/Sources/Scene/AllTab/AllTabViewController.swift
+++ Projects/Presentation/Sources/Scene/AllTab/AllTabViewController.swift
@@ -10,11 +10,84 @@ import Core
 import DesignSystem
 
 public class AllTabViewController: BaseViewController<AllTabViewModel> {
-
+    
+    private let navigationBar = PiCKMainNavigationBar()
+    private let profileView = PiCKProfileView()
+    private let helpSectionView = HelpSectionView()
+    private let accountSectionView = AccountSectionView()
+    
     public override func attribute() {
         super.attribute()
         
-        view.backgroundColor = .background
+        navigationController?.isNavigationBarHidden = true
+    }
+    public override func bindAction() {
+        navigationBar.viewSettingButtonTap
+            .bind(onNext: { [weak self] in
+                let vc = PiCKBottomSheetAlert(type: .viewType)
+                
+                let customDetents = UISheetPresentationController.Detent.custom(
+                    identifier: .init("sheetHeight")
+                ) { _ in
+                    return 252
+                }
+                
+                if let sheet = vc.sheetPresentationController {
+                    sheet.detents = [customDetents]
+                }
+                self?.present(vc, animated: true)
+            }).disposed(by: disposeBag)
+        navigationBar.displayModeButtonTap
+            .bind(onNext: { [weak self] in
+                let vc = PiCKBottomSheetAlert(type: .displayMode)
+                
+                vc.clickModeButton = { data in
+                    UserDefaultsManager.shared.set(to: data, forKey: .displayMode)
+                    let rawValue = UserDefaultsManager.shared.get(forKey: .displayMode) as! Int
+                    UIView.transition(with: self!.view, duration: 0.7, options: .transitionCrossDissolve) {
+                        self?.view.window?.overrideUserInterfaceStyle = UIUserInterfaceStyle(rawValue: rawValue) ?? .unspecified
+                    }
+                }
+                
+                let customDetents = UISheetPresentationController.Detent.custom(
+                    identifier: .init("sheetHeight")
+                ) { _ in
+                    return 228
+                }
+                
+                if let sheet = vc.sheetPresentationController {
+                    sheet.detents = [customDetents]
+                }
+                self?.present(vc, animated: true)
+            }).disposed(by: disposeBag)
+    }
+    
+    public override func addView() {
+        [
+            navigationBar,
+            profileView,
+            helpSectionView,
+            accountSectionView
+        ].forEach { view.addSubview($0) }
+    }
+        
+    public override func setLayout() {
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        profileView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).offset(36)
+            $0.leading.trailing.equalToSuperview()
+        }
+        helpSectionView.snp.makeConstraints {
+            $0.top.equalTo(profileView.snp.bottom).offset(32)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        accountSectionView.snp.makeConstraints {
+            $0.top.equalTo(helpSectionView.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
     }
 
 }
