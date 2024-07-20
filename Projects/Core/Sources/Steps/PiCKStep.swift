dec248a56b41dfd29fb0ad92a75c25c1656ce285
Choyoungjun <cyj070513@gmail.com>
2024-07-21 05:01:38 +0900

    ADD :: Steps

--- Projects/Core/Sources/Steps/PiCKStep.swift
+++ Projects/Core/Sources/Steps/PiCKStep.swift
@@ -3,29 +3,35 @@ import Foundation
 import RxFlow
 
 public enum PiCKStep: Step {
-    
     //start
     case onboardingIsRequired
     case loginIsRequired
-    
+
+    //tab
     case tabIsRequired
-    
+
     //home
     case homeIsRequired
     case alertIsRequired
-    
+
     //schoolMeal
     case schoolMealIsRequired
-    
+
     //apply
     case applyIsRequired
-    
+
     //schedule
     case scheduleIsRequired
-    
+
     //allTab
     case allTabIsRequired
-    
+    case noticeIsRequired
+    case noitceDetailIsRequired
+    case selfStudyIsRequired
+    case bugIsRequired
+    case myPageIsRequired
+    case logoutIsRequired
+
     //test
     case testIsRequired
 
