b0c70b596dffaa8b7a4c0d72baba527dd875cbdb
Choyoungjun <cyj070513@gmail.com>
2024-07-21 05:00:45 +0900

    ADD :: DI 세팅

--- Projects/Data/Sources/DI/DataSourceAssembly.swift
+++ Projects/Data/Sources/DI/DataSourceAssembly.swift
@@ -13,6 +13,46 @@ public final class DataSourceAssembly: Assembly {
     }
 
     public func assemble(container: Container) {
-       
+        container.register(AuthDataSource.self) { resolver in
+            AuthDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+        container.register(ClassroomDataSource.self) { resolver in
+            ClassroomDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+        container.register(MainDataSource.self) { resolver in
+            MainDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+        container.register(NoticeDataSource.self) { resolver in
+            NoticeDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+//        container.register(OutingDataSource.self) { resolver in
+//            OutingDataSourceImpl(keychain: self.keychain(resolver))
+//        }
+
+        container.register(ProfileDataSource.self) { resolver in
+            ProfileDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+//        container.register(ScheduleDataSource.self) { resolver in
+//            ScheduleDataSourceImpl(keychain: self.keychain(resolver))
+//        }
+
+        container.register(SchoolMealDataSource.self) { resolver in
+            SchoolMealDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+        container.register(SelfStudyTeacherDataSource.self) { resolver in
+            SelfStudyTeacherDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
+        container.register(WeekendMealDataSource.self) { resolver in
+            WeekendMealDataSourceImpl(keychain: self.keychain(resolver))
+        }
+
     }
+
 }
