cae3548aea2a00f6634c1cf7bebb2e2d32cfaac8
Choyoungjun <cyj070513@gmail.com>
2024-07-21 05:08:46 +0900

    ADD :: 알림 뷰

--- Projects/Presentation/Sources/Scene/Alert/AlertViewController.swift
+++ Projects/Presentation/Sources/Scene/Alert/AlertViewController.swift
@@ -11,10 +11,70 @@ import DesignSystem
 
 public class AlertViewController: BaseViewController<AlertViewModel> {
 
-    public override func attribute() {
-        super.attribute()
-        
-        view.backgroundColor = .red
+    private let newAlertLabel = UILabel().then {
+        $0.text = "읽지 않은 알림 (0)"
+        $0.textColor = .modeBlack
+        $0.font = .subTitle2
+    }
+    private let readAllButton = UIButton(type: .system).then {
+        $0.setTitle("모두 읽음", for: .normal)
+        $0.setTitleColor(.main500, for: .normal)
+        $0.titleLabel?.font = .subTitle2
+    }
+    private lazy var alertStackView = UIStackView(arrangedSubviews: [
+        newAlertLabel,
+        readAllButton
+    ]).then {
+        $0.axis = .horizontal
+        $0.distribution = .equalSpacing
+    }
+    private lazy var alertTableView = UITableView().then {
+        $0.backgroundColor = .background
+        $0.rowHeight = 71
+        $0.separatorStyle = .none
+        $0.register(
+            AlertTableViewCell.self,
+            forCellReuseIdentifier: AlertTableViewCell.identifier
+        )
+        $0.delegate = self
+        $0.dataSource = self
+    }
+    
+    public override func configureNavigationBar() {
+        navigationController?.isNavigationBarHidden = false
+    }
+    public override func addView() {
+        [
+            alertStackView,
+            alertTableView
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        alertStackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        alertTableView.snp.makeConstraints {
+            $0.top.equalTo(alertStackView.snp.bottom).offset(18)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
+}
+
+extension AlertViewController: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AlertTableViewCell.identifier, for: indexPath) as? AlertTableViewCell 
+        else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        cell.setup(title: "test", daysAgo: "5")
+        return cell
+    }
+    
 }
