557adf5e9d1b45b47bf88071a39c6ee01d812443
Choyoungjun <cyj070513@gmail.com>
2024-07-21 05:08:33 +0900

    ADD :: 공지 뷰 이동

--- Projects/Presentation/Sources/Scene/AllTab/AllTabViewModel.swift
+++ Projects/Presentation/Sources/Scene/AllTab/AllTabViewModel.swift
@@ -14,10 +14,17 @@ public class AllTabViewModel: BaseViewModel, Stepper {
     
     public init() {}
     
-    public struct Input {}
+    public struct Input {
+        let clickNoticeTab: Observable<IndexPath>
+    }
     public struct Output {}
     
     public func transform(input: Input) -> Output {
+        input.clickNoticeTab
+            .map { _ in PiCKStep.noticeIsRequired }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+
         return Output()
     }
     
