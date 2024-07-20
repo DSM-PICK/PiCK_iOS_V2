cec431f89a1c987a9ae5bba2935eaae423432627
Choyoungjun <cyj070513@gmail.com>
2024-07-21 05:00:45 +0900

    ADD :: DI 세팅

--- Projects/Data/Sources/DI/RepositoryAssembly.swift
+++ Projects/Data/Sources/DI/RepositoryAssembly.swift
@@ -8,6 +8,45 @@ public final class RepositoryAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
-        
+        container.register(AuthRepository.self) { resolver in
+            AuthRepositoryImpl(remoteDataSource: resolver.resolve(AuthDataSource.self)!)
+        }
+
+//        container.register(ClassroomRepository.self) { resolver in
+//            ClassroomRepositoryImpl(remoteDataSource: resolver.resolve(ClassroomDataSource.self)!)
+//        }
+
+//        container.register(MainRepository.self) { resolver in
+//            m(remoteDataSource: resolver.resolve(AuthDataSource.self)!)
+//        }
+
+        container.register(NoticeRepository.self) { resolver in
+            NoticeRepositoryImpl(remoteDataSource: resolver.resolve(NoticeDataSource.self)!)
+        }
+
+//        container.register(OutingRepository.self) { resolver in
+//            OutingRepositoryImpl(remoteDataSource: resolver.resolve(AuthDataSource.self)!)
+//        }
+
+        container.register(ProfileRepository.self) { resolver in
+            ProfileRepositoryImpl(remoteDataSource: resolver.resolve(ProfileDataSource.self)!)
+        }
+
+//        container.register(ScheduleRepository.self) { resolver in
+//            ScheduleRepositoryImpl(remoteDataSource: resolver.resolve(ScheduleDataSource.self)!)
+//        }
+
+        container.register(SchoolMealRepository.self) { resolver in
+            SchoolMealRepositoryImpl(remoteDataSource: resolver.resolve(SchoolMealDataSource.self)!)
+        }
+
+        container.register(SelfStudyRepository.self) { resolver in
+            SelfStudyTeacherRepositoryImpl(remoteDataSource: resolver.resolve(SelfStudyTeacherDataSource.self)!)
+        }
+
+//        container.register(WeekendMealRepository.self) { resolver in
+//            WeekendMealRepositoryImpl(remoteDataSource: resolver.resolve(WeekenDataSource.self)!)
+//        }
+
     }
 }
--- Projects/Data/Sources/DI/UseCaseAssembly.swift
+++ Projects/Data/Sources/DI/UseCaseAssembly.swift
@@ -8,6 +8,14 @@ public final class UseCaseAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
-       
+        container.register(LoginUseCase.self) { resolver in
+            LoginUseCase(repository: resolver.resolve(AuthRepository.self)!)
+        }
+
+//        container.register(VerifyAuthCodeUseCase.self) { resolver in
+//            VerifyAuthCodeUseCase(
+//                authRepository: resolver.resolve(AuthRepository.self)!
+//            )
+//        }
     }
 }
