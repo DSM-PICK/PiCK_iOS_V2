b03683da53642b1d02ab624c0bf602e17b87419f
Choyoungjun <cyj070513@gmail.com>
2024-07-24 01:56:53 +0900

    ADD :: 상세 공지 뷰

--- Projects/Presentation/Sources/Scene/Notice/Detail/NoticeDetailViewController.swift
+++ Projects/Presentation/Sources/Scene/Notice/Detail/NoticeDetailViewController.swift
@@ -10,9 +10,88 @@ import Core
 import DesignSystem
 
 public class NoticeDetailViewController: BaseViewController<NoticeDetailViewModel> {
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .modeBlack
+        $0.font = .subTitle1
+    }
+    private let dateLabel = UILabel().then {
+        $0.textColor = .gray500
+        $0.font = .body1
+    }
+    private let teacherNameLabel = UILabel().then {
+        $0.textColor = .gray500
+        $0.font = .body1
+    }
+    private lazy var subTextStackView = UIStackView(arrangedSubviews: [
+        dateLabel,
+        teacherNameLabel
+    ]).then {
+        $0.axis = .horizontal
+        $0.distribution = .equalSpacing
+    }
+    private let lineView = UIView().then {
+        $0.backgroundColor = .gray50
+    }
+
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let mainView = UIView()
+    private let contentLabel = UILabel().then {
+        $0.textColor = .modeBlack
+        $0.font = .body1
+    }
 
     public override func attribute() {
-        view.backgroundColor = .red
+        super.attribute()
+
+        navigationTitleText = "공지사항"
     }
     
+    public override func addView() {
+        [
+            titleLabel,
+            subTextStackView,
+            lineView,
+            scrollView
+        ].forEach { view.addSubview($0) }
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(mainView)
+        
+        mainView.addSubview(contentLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        subTextStackView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(subTextStackView.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(1)
+        }
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom)
+            $0.left.right.bottom.equalToSuperview()
+        }
+        contentView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalTo(self.view)
+        }
+        mainView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalTo(self.view.frame.height * 1.2)
+        }
+        contentLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.left.right.equalToSuperview().inset(24)
+        }
+
+    }
+
 }
