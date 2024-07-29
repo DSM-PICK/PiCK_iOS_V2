951b5a2176ebb6c3837d8a946961bb42f62b3a7d
Choyoungjun <cyj070513@gmail.com>
2024-07-29 09:53:23 +0900

    REFACT :: UserDefault 옵셔널 처리

--- Projects/Core/Sources/Storage/UserDefault/UserDefault.swift
+++ Projects/Core/Sources/Storage/UserDefault/UserDefault.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public protocol UserDefault {
     func set<T>(to: T, forKey: UserDefaultKeys)
-    func get(forKey: UserDefaultKeys) -> Any
+    func get(forKey: UserDefaultKeys) -> Any?
 }
