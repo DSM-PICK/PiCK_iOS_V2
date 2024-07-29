b097d350107cc414ada5ac4955edaa07bdd07953
Choyoungjun <cyj070513@gmail.com>
2024-07-29 14:47:59 +0900

    ADD :: 급식 뷰

---
+++ Projects/Presentation/Sources/Scene/SchoolMeal/Cell/SchoolMealCollectionViewCell.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import Core
+import DesignSystem
+
+public class SchoolMealCollectionViewCell: BaseCollectionViewCell {
+    static let identifier = "SchoolMealTableViewCell"
+
+    private let mealTimeLabel = PiCKLabel(
+        textColor: .main700,
+        font: .subTitle1
+    )
+    private let menuLabel = PiCKLabel(
+        textColor: .modeBlack,
+        font: .label1,
+        numberOfLines: 0
+    )
+
+    public func setup(
+        mealTime: String,
+        menu: String
+    ) {
+        self.mealTimeLabel.text = mealTime
+        self.menuLabel.text = menu
+    }
+
+    public override func attribute() {
+        self.backgroundColor = .main50
+        self.layer.cornerRadius = 8
+        self.layer.masksToBounds = true
+    }
+    public override func layout() {
+        [
+            mealTimeLabel,
+            menuLabel
+        ].forEach { self.addSubview($0) }
+
+        mealTimeLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(40)
+        }
+        menuLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(40)
+        }
+    }
+
+}
--- Projects/Presentation/Sources/Scene/SchoolMeal/SchoolMealViewController.swift
+++ Projects/Presentation/Sources/Scene/SchoolMeal/SchoolMealViewController.swift
@@ -10,11 +10,80 @@ import Core
 import DesignSystem
 
 public class SchoolMealViewController: BaseViewController<SchoolMealViewModel> {
-
-    public override func attribute() {
-        super.attribute()
-        
-        view.backgroundColor = .background
+    private lazy var navigationBar = PiCKMainNavigationBar(view: self)
+    private lazy var schoolMealCalendarView = PiCKCalendar(selectedDate: Date(), type: .week, presentViewController: self)
+    private let dateLabel = PiCKLabel(
+        text: "오늘 4월 13일",
+        textColor: .modeBlack,
+        font: .heading4
+    ).then {
+        $0.changePointColor(targetString: "오늘", color: .main500)
     }
+    private lazy var collectionViewFlowLayout = UICollectionViewFlowLayout().then {
+        $0.itemSize = .init(width: self.view.frame.width - 48, height: 154)
+        $0.minimumLineSpacing = 20
+    }
+    private lazy var schoolMealCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: collectionViewFlowLayout
+    ).then {
+        $0.backgroundColor = .background
+        $0.register(
+            SchoolMealCollectionViewCell.self,
+            forCellWithReuseIdentifier: SchoolMealCollectionViewCell.identifier
+        )
+        $0.delegate = self
+        $0.dataSource = self
+    }
+
+    public override func configureNavgationBarLayOutSubviews() {
+        super.configureNavgationBarLayOutSubviews()
+
+        navigationController?.isNavigationBarHidden = true
+    }
+    public override func addView() {
+        [
+            navigationBar,
+            schoolMealCalendarView,
+            dateLabel,
+            schoolMealCollectionView
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        schoolMealCalendarView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(150)
+        }
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(schoolMealCalendarView.snp.bottom).offset(36)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        schoolMealCollectionView.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(36)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview()
+        }
+    }
+
+}
+
+extension SchoolMealViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SchoolMealCollectionViewCell.identifier, for: indexPath) as? SchoolMealCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.setup(mealTime: "조식", menu: "ㄹ너ㅣㅏ\nㅓㄹㅇ날ㄴ\nㄹ어나ㅣ")
+        return cell
+    }
+
 
 }
