7fa97231e4f6f725b91681ad4e58efc9a9b90ef7
Choyoungjun <cyj070513@gmail.com>
2024-07-29 14:48:19 +0900

    ADD :: 일정 뷰

---
+++ Projects/Presentation/Sources/Scene/Schedule/AcademicSchedule/View/AcademicScheduleView.swift
@@ -0,0 +1,9 @@
+//
+//  AcademicScheduleView.swift
+//  Presentation
+//
+//  Created by 조영준 on 7/28/24.
+//  Copyright © 2024 com.pick-v2.app. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Presentation/Sources/Scene/Schedule/TimeTable/View/TimeTableView.swift
@@ -0,0 +1,16 @@
+//import UIKit
+//
+//import SnapKit
+//import Then
+//
+//import RxSwift
+//import RxCocoa
+//
+//import Core
+//import DesignSystem
+//
+//public class TimeTableView: BaseView {
+//
+//
+//
+//}
