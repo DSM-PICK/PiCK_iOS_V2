8f3c06d5bad7642addb84b2189a8fe4ac78f8987
Choyoungjun <cyj070513@gmail.com>
2024-07-29 09:51:50 +0900

    ADD :: BaseLabel

---
+++ Projects/Core/Sources/Base/BaseLabel.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+open class BaseLabel: UILabel {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        attribute()
+    }
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open func attribute() {
+        //UILable 관련 설정을 하는 함수
+    }
+
+}
