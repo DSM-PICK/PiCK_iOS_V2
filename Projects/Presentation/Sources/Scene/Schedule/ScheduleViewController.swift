fd315b980c3e1562720811ee202ab1ac424fefbf
Choyoungjun <cyj070513@gmail.com>
2024-07-29 14:48:19 +0900

    ADD :: 일정 뷰

--- Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
+++ Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
@@ -10,11 +10,25 @@ import Core
 import DesignSystem
 
 public class ScheduleViewController: BaseViewController<ScheduleViewModel> {
+    private lazy var navigationBar = PiCKMainNavigationBar(view: self)
 
-    public override func attribute() {
-        super.attribute()
-        
-        view.backgroundColor = .background
+//    private let scheduleSegmentedControl = PiCKSegmentedControl(items: [])
+
+    public override func configureNavgationBarLayOutSubviews() {
+        super.configureNavgationBarLayOutSubviews()
+
+        navigationController?.isNavigationBarHidden = true
+    }
+    public override func addView() {
+        [
+            navigationBar
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
     }
 
 }
