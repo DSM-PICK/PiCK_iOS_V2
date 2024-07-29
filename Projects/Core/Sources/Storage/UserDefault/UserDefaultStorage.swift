02dc7c8878fcad2db34150fd00c2efa216f56d00
Choyoungjun <cyj070513@gmail.com>
2024-07-29 09:53:23 +0900

    REFACT :: UserDefault 옵셔널 처리

--- Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
+++ Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 public enum UserDefaultKeys: String {
+    case homeViewMode = "viewMode"
     case displayMode = "displayMode"
 }
 
@@ -10,11 +11,9 @@ public struct UserDefaultsManager: UserDefault {
     
     public func set<T>(to: T, forKey: UserDefaultKeys) {
         UserDefaults.standard.set(to, forKey: forKey.rawValue)
-        print("UserDefaultsManager: save \(forKey) complete")
-        print("\(UserDefaultsManager.shared.get(forKey: .displayMode))")
     }
     
-    public func get(forKey: UserDefaultKeys) -> Any {
+    public func get(forKey: UserDefaultKeys) -> Any? {
         return UserDefaults.standard.object(forKey: forKey.rawValue)!
     }
     
