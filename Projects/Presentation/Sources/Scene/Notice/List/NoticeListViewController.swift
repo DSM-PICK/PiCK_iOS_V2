faee61bbfb6e2e9e5d392286cdb87dd58625f50c
Choyoungjun <cyj070513@gmail.com>
2024-07-29 14:48:34 +0900

    REFACT :: 정리

--- Projects/Presentation/Sources/Scene/Notice/List/NoticeListViewController.swift
+++ Projects/Presentation/Sources/Scene/Notice/List/NoticeListViewController.swift
@@ -13,11 +13,6 @@ public class NoticeListViewController: BaseViewController<NoticeListViewModel> {
     
     private let noticeDetailRelay = PublishRelay<Void>()
 
-    private let navigationTitleLabel = UILabel().then {
-        $0.text = "공지사항"
-        $0.textColor = .modeBlack
-        $0.font = .body1
-    }
     private let bannerView = UIImageView(image: .noticeBanner)
     private lazy var bottomCollectionViewFlowLayout = UICollectionViewFlowLayout().then {
         $0.scrollDirection = .vertical
@@ -34,35 +29,35 @@ public class NoticeListViewController: BaseViewController<NoticeListViewModel> {
             NoticeCollectionViewCell.self,
             forCellWithReuseIdentifier: NoticeCollectionViewCell.identifier
         )
-//        $0.delegate = self
-//        $0.dataSource = self
+        $0.delegate = self
+        $0.dataSource = self
     }
-    public override func configureNavigationBar() {
-        navigationItem.titleView = navigationTitleLabel
-        navigationController?.isNavigationBarHidden = false
+    public override func attribute() {
+        super.attribute()
+        navigationTitleText = "공지사항"
     }
     let dd = BehaviorRelay<[NoticeListEntityElement]>(value: [
         NoticeListEntityElement(id: UUID(), title: "test", createAt: "fjdskl")
     ])
-    public override func bind() {
-        let input = NoticeListViewModel.Input(
-            clickNotice: noticeDetailRelay.asObservable()
-        )
-        _ = viewModel.transform(input: input)
-
-        dd.bind(to: noticeCollectionView.rx.items(
-            cellIdentifier: NoticeCollectionViewCell.identifier,
-            cellType: NoticeCollectionViewCell.self
-        )) { row, item, cell in
-            cell.setup(title: "[중요] 오리엔테이션날 일정 안내", daysAgo: "1일전", isNew: false)
-        }.disposed(by: disposeBag)
-        
-        noticeCollectionView.rx.modelSelected(NoticeListEntityElement.self)
-            .bind(onNext: { index in
-                self.noticeDetailRelay.accept(())
-                print(index)
-            }).disposed(by: disposeBag)
-    }
+//    public override func bind() {
+//        let input = NoticeListViewModel.Input(
+//            clickNotice: noticeDetailRelay.asObservable()
+//        )
+//        _ = viewModel.transform(input: input)
+//
+//        dd.bind(to: noticeCollectionView.rx.items(
+//            cellIdentifier: NoticeCollectionViewCell.identifier,
+//            cellType: NoticeCollectionViewCell.self
+//        )) { row, item, cell in
+//            cell.setup(title: "[중요] 오리엔테이션날 일정 안내", daysAgo: "1일전", isNew: false)
+//        }.disposed(by: disposeBag)
+//        
+//        noticeCollectionView.rx.modelSelected(NoticeListEntityElement.self)
+//            .bind(onNext: { index in
+//                self.noticeDetailRelay.accept(())
+//                print(index)
+//            }).disposed(by: disposeBag)
+//    }
     public override func addView() {
         [
             bannerView,
@@ -82,33 +77,33 @@ public class NoticeListViewController: BaseViewController<NoticeListViewModel> {
 
 }
 
-//extension NoticeListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-//    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//    
-//    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView
-//            .dequeueReusableCell(
-//                withReuseIdentifier: NoticeCollectionViewCell.identifier,
-//                for: indexPath
-//        ) as? NoticeCollectionViewCell else {
-//            return UICollectionViewCell()
-//        }
-//        
-//        cell.setup(title: "[중요] 오리엔테이션날 일정 안내", daysAgo: "1일전", isNew: false)
-//        return cell
-//    }
-//    
-//    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        _ = collectionView.dequeueReusableCell(withReuseIdentifier: NoticeCollectionViewCell.identifier, for: indexPath)
-//        
+extension NoticeListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+            .dequeueReusableCell(
+                withReuseIdentifier: NoticeCollectionViewCell.identifier,
+                for: indexPath
+        ) as? NoticeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setup(title: "[중요] 오리엔테이션날 일정 안내", daysAgo: "1일전", isNew: false)
+        return cell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        _ = collectionView.dequeueReusableCell(withReuseIdentifier: NoticeCollectionViewCell.identifier, for: indexPath)
+        
 //        noticeDetailRelay.accept(())
-//        print("DFjskl")
-//        
-//    }
-//    
-//}
+        self.navigationController?.pushViewController(NoticeDetailViewController(viewModel: NoticeDetailViewModel()), animated: true)
+        
+    }
+    
+}
 
 public struct NoticeListEntityElement {
     public let id: UUID
