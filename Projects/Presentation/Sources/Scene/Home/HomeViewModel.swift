8b33a8f1372bcb7d34a211cbc941aae5413d2af1
Choyoungjun <cyj070513@gmail.com>
2024-07-29 14:50:22 +0900

    FEAT :: 뷰 이동 및 리펙토링

--- Projects/Presentation/Sources/Scene/Home/HomeViewModel.swift
+++ Projects/Presentation/Sources/Scene/Home/HomeViewModel.swift
@@ -15,17 +15,33 @@ public class HomeViewModel: BaseViewModel, Stepper {
     public init() {}
     
     public struct Input {
-        let clickAlertButton: Observable<Void>
+        let viewWillApper: Observable<Void>
+        let clickAlert: Observable<Void>
+        let clickViewMore: Observable<Void>
     }
-    public struct Output {}
-    
+    public struct Output {
+        let viewMode: Signal<HomeViewType>
+    }
+
+    private let viewModeData = PublishRelay<HomeViewType>()
+
     public func transform(input: Input) -> Output {
-        
-        input.clickAlertButton
+        input.viewWillApper
+            .map { _ in UserDefaultsManager.shared.get(forKey: .homeViewMode)}
+            .subscribe(onNext: { [weak self] data in
+                self?.viewModeData.accept(data as! HomeViewType)
+            }).disposed(by: disposeBag)
+
+        input.clickAlert
             .map { PiCKStep.alertIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
-        return Output()
+
+        input.clickViewMore
+            .map { PiCKStep.noticeIsRequired }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        return Output(viewMode: viewModeData.asSignal())
     }
     
 }
