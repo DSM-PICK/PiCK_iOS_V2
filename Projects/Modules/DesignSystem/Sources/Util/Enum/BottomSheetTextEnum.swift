7ecfb86851cdcd781ae69346c782ba9140af82be
Choyoungjun <cyj070513@gmail.com>
2024-07-30 16:53:21 +0900

    ADD :: BottomSheet Text

---
+++ Projects/Modules/DesignSystem/Sources/Util/Enum/BottomSheetTextEnum.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public enum BottomSheetTextEnum: String {
+    //MARK: homeViewMode
+    case homeViewModeExplainText = "픽은 메인 페이지를 커스텀 할 수 있어요!"
+    //timeTable
+    case timeTableSubText = "메인에서 오늘의 급식 보기"
+    case timeTableExpainText = "지금은 시간표로 설정되어 있어요"
+    case timeTableButtonText = "급식으로 설정하기"
+    //schoolMeal
+    case schoolMealSubText = "메인에서 오늘의 시간표 보기"
+    case schoolMealExpainText = "지금은 급식으로 설정되어 있어요"
+    case schoolMealButtonText = "시간표로 설정하기"
+    //MARK: displayMode
+    case displayModeExplainText = "픽은 라이트 모드 또는 다크 모드로 변경할 수 있어요"
+    //light
+    case lightSubExplainText = "픽을 다크 모드로 설정 하시겠어요?"
+    case ligthButtonext = "다크 모드로 설정하기"
+    //dark
+    case darkSubExplainText = "픽을 라이트 모드로 설정 하시겠어요?"
+    case darkButtonext = "라이트 모드로 설정하기"
+}
