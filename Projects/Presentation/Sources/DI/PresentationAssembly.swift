bc32820f81e84eb6889fc26659bd09baffbbb953
Choyoungjun <cyj070513@gmail.com>
2024-07-30 16:59:38 +0900

    ADD :: Presentation DI

--- Projects/Presentation/Sources/DI/PresentationAssembly.swift
+++ Projects/Presentation/Sources/DI/PresentationAssembly.swift
@@ -9,7 +9,7 @@ public final class PresentationAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
-        //onboarding
+        //MARK: Onboarding
         container.register(OnboardingViewController.self) { resolver in
             OnboardingViewController(viewModel: resolver.resolve(OnboardingViewModel.self)!)
         }
@@ -17,15 +17,63 @@ public final class PresentationAssembly: Assembly {
             OnboardingViewModel()
         }
         
-        //login
+        //MARK: Login
         container.register(LoginViewController.self) { resolver in
             LoginViewController(viewModel: resolver.resolve(LoginViewModel.self)!)
         }
         container.register(LoginViewModel.self) { resolver in
-            LoginViewModel(loginUseCase: resolver.resolve(LoginUseCase.self)!)
+            LoginViewModel(
+                loginUseCase: resolver.resolve(LoginUseCase.self)!
+            )
         }
-        
-        //notice
+
+        //MARK: Home
+        container.register(HomeViewController.self) { resolver in
+            HomeViewController(viewModel: resolver.resolve(HomeViewModel.self)!)
+        }
+        container.register(HomeViewModel.self) { resolver in
+            HomeViewModel()
+        }
+        //Alert
+        container.register(AlertViewController.self) { resolver in
+            AlertViewController(viewModel: resolver.resolve(AlertViewModel.self)!)
+        }
+        container.register(AlertViewModel.self) { resolver in
+            AlertViewModel()
+        }
+
+        //MARK: SchoolMeal
+        container.register(SchoolMealViewController.self) { resolver in
+            SchoolMealViewController(viewModel: resolver.resolve(SchoolMealViewModel.self)!)
+        }
+        container.register(SchoolMealViewModel.self) { resolver in
+            SchoolMealViewModel()
+        }
+
+        //MARK: Apply
+        container.register(ApplyViewController.self) { resolver in
+            ApplyViewController(viewModel: resolver.resolve(ApplyViewModel.self)!)
+        }
+        container.register(ApplyViewModel.self) { resolver in
+            ApplyViewModel()
+        }
+
+        //MARK: Schedule
+        container.register(ScheduleViewController.self) { resolver in
+            ScheduleViewController(viewModel: resolver.resolve(ScheduleViewModel.self)!)
+        }
+        container.register(ScheduleViewModel.self) { resolver in
+            ScheduleViewModel()
+        }
+
+        //MARK: AllTab
+        container.register(AllTabViewController.self) { resolver in
+            AllTabViewController(viewModel: resolver.resolve(AllTabViewModel.self)!)
+        }
+        container.register(AllTabViewModel.self) { resolver in
+            AllTabViewModel()
+        }
+        //Notice
         container.register(NoticeListViewController.self) { resolver in
             NoticeListViewController(viewModel: resolver.resolve(NoticeListViewModel.self)!)
         }
@@ -39,16 +87,21 @@ public final class PresentationAssembly: Assembly {
         container.register(NoticeDetailViewModel.self) { resolver in
             NoticeDetailViewModel()
         }
-        
-        //allTab
-        //bug
+        //SelfStudy
+        container.register(SelfStudyViewController.self) { resolver in
+            SelfStudyViewController(viewModel: resolver.resolve(SelfStudyViewModel.self)!)
+        }
+        container.register(SelfStudyViewModel.self) { resolver in
+            SelfStudyViewModel()
+        }
+        //Bug
         container.register(BugReportViewController.self) { resolver in
             BugReportViewController(viewModel: resolver.resolve(BugReportViewModel.self)!)
         }
         container.register(BugReportViewModel.self) { resolver in
             BugReportViewModel()
         }
-        //myPage
+        //MyPage
         container.register(MyPageViewController.self) { resolver in
             MyPageViewController(viewModel: resolver.resolve(MyPageViewModel.self)!)
         }
