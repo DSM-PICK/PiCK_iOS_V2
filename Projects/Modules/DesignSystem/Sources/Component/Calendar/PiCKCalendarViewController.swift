4b83f998250d357f14ca6bc115f29fd5bacd1a23
Choyoungjun <cyj070513@gmail.com>
2024-07-31 09:53:21 +0900

    ADD :: [#1] 캘린더

---
+++ Projects/Modules/DesignSystem/Sources/Component/Calendar/PiCKCalendarViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+
+public class PiCKCalendarViewController: UIViewController {
+    private let disposeBag = DisposeBag()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 20
+    }
+
+//    private func attribute() {
+//        
+//    }
+//    private func bind() {
+//        
+//        
+//    }
+//    private func addView() {
+//        
+//    }
+//    private func setLayout() {
+//        
+//    }
+
+}
