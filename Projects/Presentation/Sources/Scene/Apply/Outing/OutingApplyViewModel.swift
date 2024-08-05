587fa211bd02f824fb2ae0c6bacb4872b2ad3d15
Choyoungjun <cyj070513@gmail.com>
2024-08-05 19:10:45 +0900

    FEAT :: [#16] 외출 신청 뷰

--- Projects/Presentation/Sources/Scene/Apply/Outing/OutingApplyViewModel.swift
+++ Projects/Presentation/Sources/Scene/Apply/Outing/OutingApplyViewModel.swift
@@ -14,11 +14,36 @@ public class OutingApplyViewModel: BaseViewModel, Stepper {
     
     public init() {}
     
-    public struct Input {}
-    public struct Output {}
-    
+    public struct Input {
+        let startTime: Observable<String>
+        let clickStartTimeButton: Observable<Void>
+        let endTime: Observable<String>
+        let clickEndTimeButton: Observable<Void>
+        let reasonText: Observable<String>
+        let clickApplyButton: Observable<Void>
+    }
+    public struct Output {
+        let isApplyButtonEnable: Signal<Bool>
+    }
+
     public func transform(input: Input) -> Output {
-        return Output()
+        let info = Observable.combineLatest(
+            input.reasonText,
+            input.startTime,
+            input.endTime
+        )
+        
+        let isApplyButtonEnable = info.map { reason, startTime, endTime -> Bool in !reason.isEmpty && !startTime.isEmpty && !endTime.isEmpty && startTime < endTime
+        }
+        
+        input.clickStartTimeButton.asObservable()
+            .map { PiCKStep.timeSelectAlertIsRequired }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        input.clickEndTimeButton.asObservable()
+            
+        
+        return Output(isApplyButtonEnable: isApplyButtonEnable.asSignal(onErrorJustReturn: false))
     }
     
 }
