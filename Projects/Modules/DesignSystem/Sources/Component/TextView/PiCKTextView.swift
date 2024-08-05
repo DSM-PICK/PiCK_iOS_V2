5c140c12b895fc49299c445b4f5a479464e2554f
Choyoungjun <cyj070513@gmail.com>
2024-08-05 14:48:55 +0900

    REFACT :: [#16] PiCKTextView 리펙토링

--- Projects/Modules/DesignSystem/Sources/Component/TextView/PiCKTextView.swift
+++ Projects/Modules/DesignSystem/Sources/Component/TextView/PiCKTextView.swift
@@ -8,53 +8,73 @@ import RxCocoa
 
 import Core
 
-public class PiCKTextView: BaseTextView {
-    
+public class PiCKTextView: BaseView {
+    public var isEdit = BehaviorRelay<Bool>(value: false)
+
+    private let titleLabel = PiCKLabel(
+        textColor: .modeBlack,
+        font: .label1
+    )
+    private let textView = UITextView().then {
+        $0.textColor = .modeBlack
+        $0.font = .caption2
+        $0.textContainer.maximumNumberOfLines = 8
+        $0.backgroundColor = .gray50
+        $0.layer.cornerRadius = 4
+        $0.autocorrectionType = .no
+        $0.autocapitalizationType = .none
+        $0.textContainerInset = UIEdgeInsets(top: 14, left: 16, bottom: 16, right: 16)
+    }
     private let placeholderLabel = PiCKLabel(
-        text: "내용을 입력해주세요",
         textColor: .gray500,
         font: .caption2
     )
 
     public init(
+        title: String,
         placeholder: String
     ) {
+        self.titleLabel.text = title
         self.placeholderLabel.text = placeholder
-        super.init(frame: .zero, textContainer: .none)
+        super.init(frame: .zero)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    public override func attribute() {
-        self.textColor = .modeBlack
-        self.font = .caption2
-        self.textContainer.maximumNumberOfLines = 0
-        self.backgroundColor = .gray50
-        self.layer.cornerRadius = 4
-        self.autocorrectionType = .no
-        self.autocapitalizationType = .none
-        self.textContainerInset = UIEdgeInsets(top: 14, left: 16, bottom: 16, right: 16)
-    }
     public override func layout() {
-        self.addSubview(placeholderLabel)
-        
+        [
+            titleLabel,
+            textView
+        ].forEach { self.addSubview($0) }
+        self.textView.addSubview(placeholderLabel)
+
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        textView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
         placeholderLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(14)
             $0.left.equalToSuperview().inset(16)
         }
     }
 
-    public override func bindActions() {
-        self.rx.didBeginEditing
+    public override func bind() {
+        self.textView.rx.didBeginEditing
             .bind(onNext: { [weak self] in
                 self?.placeholderLabel.isHidden = true
+                self?.isEdit.accept(true)
             }).disposed(by: disposeBag)
         
-        self.rx.didEndEditing
+        self.textView.rx.didEndEditing
             .bind(onNext: { [weak self] in
-                if self?.text.isEmpty == true {
+                if self?.textView.text.isEmpty == true {
                     self?.placeholderLabel.isHidden = false
+                    self?.isEdit.accept(false)
                 }
             }).disposed(by: disposeBag)
     }
