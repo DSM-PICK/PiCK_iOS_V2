0ee062ee69fffd2bcdcc2e7e1de132c4d34729aa
Choyoungjun <cyj070513@gmail.com>
2024-08-05 19:11:08 +0900

    FEAT :: [#16] 조기 귀가 신청 뷰

---
+++ Projects/Presentation/Sources/Scene/Apply/EarlyLeave/EarlyLeaveApplyViewController.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class EarlyLeaveApplyViewController: BaseViewController<EarlyLeaveApplyViewModel> {
+    private let titleLabel = PiCKLabel(text: "조기 귀가 신청", textColor: .modeBlack, font: .heading4)
+    private let explainLabel = PiCKLabel(text: "희망 귀가 시간을 선택하세요", textColor: .modeBlack, font: .label1)
+    private let startTimeSelectButton = TimeSelectButton(type: .system)
+    private let outingReasonTextView = PiCKTextView(title: "귀가 사유를 입력하세요", placeholder: "자세히 입력해주세요")
+    private let applyButton = PiCKButton(type: .system, buttonText: "신청하기", isEnabled: false)
+
+    public override func attribute() {
+        super.attribute()
+        
+        navigationTitleText = "조기 귀가 신청"
+    }
+    public override func bind() {
+        let input = EarlyLeaveApplyViewModel.Input(
+            startTime: Observable.just("fjsklfds"),
+            clickStartTimeButton: startTimeSelectButton.buttonTap.asObservable(),
+            reasonText: Observable.just("fjsklfds"),
+            clickApplyButton: applyButton.buttonTap.asObservable()
+        )
+        let output =  viewModel.transform(input: input)
+        
+        output.isApplyButtonEnable.asObservable()
+            .bind(
+                onNext: { [weak self] isEnabled in
+                    self?.applyButton.isEnabled = isEnabled
+                }
+            ).disposed(by: disposeBag)
+    }
+    public override func addView() {
+        [
+            titleLabel,
+            explainLabel,
+            startTimeSelectButton,
+            outingReasonTextView,
+            applyButton
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        explainLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(24)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        startTimeSelectButton.snp.makeConstraints {
+            $0.top.equalTo(explainLabel.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        outingReasonTextView.snp.makeConstraints {
+            $0.top.equalTo(startTimeSelectButton.snp.bottom).offset(68)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(151)
+        }
+        applyButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().inset(60)
+        }
+
+    }
+
+}
