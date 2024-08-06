32789b7f42762d904a15672f49af49ded9e50112
Choyoungjun <cyj070513@gmail.com>
2024-08-06 23:27:44 +0900

    FEAT :: [#17] WeekendMealApplyFlow

--- Projects/Flow/Sources/Apply/WeekendMeal/WeekendMealApplyFlow.swift
+++ Projects/Flow/Sources/Apply/WeekendMeal/WeekendMealApplyFlow.swift
@@ -1,1 +1,40 @@
-import Foundation
+import UIKit
+
+import RxFlow
+import Swinject
+
+import Core
+import DesignSystem
+import Presentation
+
+public class WeekendMealApplyFlow: Flow {
+    public let container: Container
+    private let rootViewController: WeekendMealApplyViewController
+    public var root: Presentable {
+        return rootViewController
+    }
+    
+    public init(container: Container) {
+        self.container = container
+        self.rootViewController = container.resolve(WeekendMealApplyViewController.self)!
+    }
+
+    public func navigate(to step: RxFlow.Step) -> RxFlow.FlowContributors {
+        guard let step = step as? PiCKStep else { return .none }
+        
+        switch step {
+        case .weekendMealApplyIsRequired:
+            return navigateToWeekendMealApply()
+        default:
+            return .none
+        }
+    }
+
+    private func navigateToWeekendMealApply() -> FlowContributors {
+        return .one(flowContributor: .contribute(
+            withNextPresentable: rootViewController,
+            withNextStepper: rootViewController.viewModel
+        ))
+    }
+
+}
