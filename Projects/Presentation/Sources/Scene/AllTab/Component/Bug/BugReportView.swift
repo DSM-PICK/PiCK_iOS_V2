7f19a5e960abcc9caf6bdf2bc670e858ac8ddf0a
Choyoungjun <cyj070513@gmail.com>
2024-08-09 22:46:20 +0900

    FEAT :: [#21] BugReportView

--- Projects/Presentation/Sources/Scene/AllTab/Component/Bug/BugReportView.swift
+++ Projects/Presentation/Sources/Scene/AllTab/Component/Bug/BugReportView.swift
@@ -12,9 +12,18 @@ public enum BugType {
 public class BugReportView: BaseView {
     private var bugType: BugType = .location
 
-    private let titleLabel = PiCKLabel(text: "어디서 버그가 발생했나요? *", textColor: .modeBlack, font: .label1)
-    private let textField = PiCKTextField(placeholder: "예: 메인, 외출 신청")
-    private let textView = PiCKTextView(title: "버그에 대해 설명해주세요. *", placeholder: "자세히 입력해주세요")
+    private let titleLabel = PiCKLabel(textColor: .modeBlack, font: .label1)
+    private let textField = PiCKTextField(placeholder: "예: 메인, 외출 신청").then {
+        $0.isHidden = true
+    }
+    private let textView = PiCKTextView(title: "버그에 대해 설명해주세요. *", pointText: "*", placeholder: "자세히 입력해주세요").then {
+        
+        $0.isHidden = true
+    }
+    private let imageView = BugImageView().then {
+        $0.isHidden = true
+    }
+
     public init(
         type: BugType
     ) {
@@ -30,7 +39,8 @@ public class BugReportView: BaseView {
         [
             titleLabel,
             textField,
-            textView
+            textView,
+            imageView
         ].forEach { self.addSubview($0) }
 
         titleLabel.snp.makeConstraints {
@@ -44,17 +54,24 @@ public class BugReportView: BaseView {
         textView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
     private func typeSetting() {
         switch bugType {
         case .location:
-            textView.isHidden = true
+            titleLabel.text = "어디서 버그가 발생했나요? *"
+            titleLabel.changePointColor(targetString: "*", color: .error)
+            textField.isHidden = false
         case .explain:
             titleLabel.isHidden = true
-            textField.isHidden = true
+            textView.isHidden = false
         case .photo:
-            break
+            titleLabel.text = "버그 사진을 첨부해주세요"
+            imageView.isHidden = false
         }
     }
 
