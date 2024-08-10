370c31986435c046b783f0e92b3635d308eb951e
Choyoungjun <cyj070513@gmail.com>
2024-08-10 22:49:14 +0900

    FEAT :: [#21] dashedLineBorder 추가

---
+++ Projects/Core/Sources/Util/UI/CALayer+.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public extension CALayer {
+    func border(color: UIColor?, width: CGFloat?) {
+        self.borderColor = color?.cgColor
+        self.borderWidth = width ?? CGFloat()
+    }
+
+    func addDotBorder(color: UIColor, lineWidth: CGFloat, dotRadius: CGFloat) {
+
+        self.sublayers?.removeAll(where: { $0.name == "DotBorder" })
+
+        let dotBorder = CAShapeLayer()
+        dotBorder.name = "DotBorder"
+        dotBorder.strokeColor = color.cgColor
+        dotBorder.fillColor = nil
+        dotBorder.lineWidth = lineWidth
+        dotBorder.lineDashPattern = [NSNumber(value: Double(dotRadius)), NSNumber(value: Double(dotRadius * 2))]
+
+        dotBorder.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.cornerRadius).cgPath
+
+        self.addSublayer(dotBorder)
+    }
+}
