bcd14be94d00371f6be0b2084db4d273912ce1b6
Choyoungjun <cyj070513@gmail.com>
2024-08-11 23:26:29 +0900

    FEAT :: [#23] DI

--- Projects/Data/Sources/DI/UseCaseAssembly.swift
+++ Projects/Data/Sources/DI/UseCaseAssembly.swift
@@ -8,8 +8,16 @@ public final class UseCaseAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
+        //MARK: Login
         container.register(LoginUseCase.self) { resolver in
             LoginUseCase(repository: resolver.resolve(AuthRepository.self)!)
         }
+
+        //MARK: AllTab
+        container.register(FetchDetailProfileUseCase.self) { resolver in
+            FetchDetailProfileUseCase(repository: resolver.resolve(ProfileRepository.self)!)
+        }
+
     }
+
 }
