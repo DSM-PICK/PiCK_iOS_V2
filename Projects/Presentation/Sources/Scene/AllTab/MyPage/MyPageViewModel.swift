3b8c8820563c98ef05ce02b9bb415aa8c611e6b2
Choyoungjun <cyj070513@gmail.com>
2024-08-11 23:33:17 +0900

    FEAT :: [#23] 마이페이지 서버통신

--- Projects/Presentation/Sources/Scene/AllTab/MyPage/MyPageViewModel.swift
+++ Projects/Presentation/Sources/Scene/AllTab/MyPage/MyPageViewModel.swift
@@ -8,17 +8,37 @@ import Core
 import Domain
 
 public class MyPageViewModel: BaseViewModel, Stepper {
-    
     private let disposeBag = DisposeBag()
     public var steps = PublishRelay<Step>()
-    
-    public init() {}
-    
-    public struct Input {}
-    public struct Output {}
-    
-    public func transform(input: Input) -> Output {
-        return Output()
+
+    private let profileUsecase: FetchDetailProfileUseCase
+
+    public init(profileUsecase: FetchDetailProfileUseCase) {
+        self.profileUsecase = profileUsecase
     }
     
+    public struct Input {
+        let viewWillAppear: Observable<Void>
+    }
+    public struct Output {
+        let profileData: Signal<DetailProfileEntity>
+    }
+
+    private let profileData = PublishRelay<DetailProfileEntity>()
+
+    public func transform(input: Input) -> Output {
+        input.viewWillAppear
+            .flatMap {
+                self.profileUsecase.execute()
+                    .catch {
+                        print($0.localizedDescription)
+                        return .never()
+                    }
+            }
+            .bind(to: profileData)
+            .disposed(by: disposeBag)
+
+        return Output(profileData: profileData.asSignal())
+    }
+
 }
