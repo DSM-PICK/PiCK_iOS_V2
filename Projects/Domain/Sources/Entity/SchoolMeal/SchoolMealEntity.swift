24807faafd6827bee2bfabaeb7fa7709637ed113
Choyoungjun <cyj070513@gmail.com>
2024-08-13 21:20:12 +0900

    CHORE :: [#36] 급식 로직 수정 중

--- Projects/Domain/Sources/Entity/SchoolMeal/SchoolMealEntity.swift
+++ Projects/Domain/Sources/Entity/SchoolMeal/SchoolMealEntity.swift
@@ -1,19 +1,35 @@
 import Foundation
 
 public struct SchoolMealEntity {
-    public let meals: SchoolMealEntityElement
-    
-    public init(meals: SchoolMealEntityElement) {
+    public let date: String
+    public let meals: [SchoolMealEntityElement]
+
+    public init(
+        date: String,
+        meals: [SchoolMealEntityElement]
+    ) {
+        self.date = date
         self.meals = meals
     }
-    
 }
 
 public struct SchoolMealEntityElement {
-    public let mealBundle: [(Int, String, [String])]
+//    public let mealBundle: [(Int, String, [String])]
+    public let breakfast: [String]
+    public let lunch: [String]
+    public let dinner: [String]
 
-    public init(mealBundle: [(Int, String, [String])]) {
-        self.mealBundle = mealBundle
+//    public init(mealBundle: [(Int, String, [String])]) {
+//        self.mealBundle = mealBundle
+//    }
+    public init(
+        breakfast: [String],
+        lunch: [String],
+        dinner: [String]
+    ) {
+        self.breakfast = breakfast
+        self.lunch = lunch
+        self.dinner = dinner
     }
-    
+
 }
