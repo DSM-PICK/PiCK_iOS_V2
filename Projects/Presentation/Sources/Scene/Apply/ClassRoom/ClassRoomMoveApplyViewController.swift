31a667e1ee06ddcf6fd566facb62077a19fde3ef
Choyoungjun <cyj070513@gmail.com>
2024-08-15 22:58:23 +0900

    FEAT :: [#42] Alert 액션 추가

--- Projects/Presentation/Sources/Scene/Apply/ClassRoom/ClassRoomMoveApplyViewController.swift
+++ Projects/Presentation/Sources/Scene/Apply/ClassRoom/ClassRoomMoveApplyViewController.swift
@@ -120,7 +120,6 @@ public class ClassRoomMoveApplyViewController: BaseViewController<ClassRoomMoveA
                     self?.classRoomMoveApplyRelay.accept(())
                 }
                 self.presentAsCustomDents(view: vc, height: 406)
-                //TODO: alert에서 배경 클릭 또는 확인 버튼만 dismiss 가능하게 uiview는 안되는걸로
             }.disposed(by: disposeBag)
         
     }
