e812423fabd95c85dde7f30a08e526da66ded5c8
Choyoungjun <cyj070513@gmail.com>
2024-08-17 23:04:10 +0900

    FEAT :: [#47] 버그제보 뷰 퍼블리싱

--- Projects/Presentation/Sources/Scene/AllTab/Bug/Cell/BugImageCollectionViewCell.swift
+++ Projects/Presentation/Sources/Scene/AllTab/Bug/Cell/BugImageCollectionViewCell.swift
@@ -3,15 +3,21 @@ import UIKit
 import SnapKit
 import Then
 
+import RxSwift
+import RxCocoa
+
 import Core
 import DesignSystem
 
 public class BugImageCollectionViewCell: BaseCollectionViewCell<Any> {
-
     static let identifier = "BugImageCollectionViewCell"
 
+    public var deleteButtonTap: ControlEvent<Void> {
+        return deleteButton.rx.tap
+    }
     private let imageView = UIImageView().then {
-        $0.image = .mainBanner
+        $0.layer.cornerRadius = 4
+        $0.layer.masksToBounds = true
     }
     private let deleteButton = UIButton(type: .system).then {
         $0.setTitle("삭제", for: .normal)
@@ -21,12 +27,20 @@ public class BugImageCollectionViewCell: BaseCollectionViewCell<Any> {
         $0.layer.cornerRadius = 8
     }
 
+    public func setup(
+        image: UIImage
+    ) {
+        self.imageView.image = image
+    }
+
     public override func attribute() {
         self.layer.cornerRadius = 4
     }
     public override func layout() {
-        self.addSubview(imageView)
-        imageView.addSubview(deleteButton)
+        [
+            imageView,
+            deleteButton
+        ].forEach { self.addSubview($0) }
         
         imageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
