b8b2bc218bd8238aeb2e2062614723cd10ac5d58
Choyoungjun <cyj070513@gmail.com>
2024-08-20 13:45:46 +0900

    REFACT :: [#50] Label 추가

--- Projects/Presentation/Sources/Scene/Apply/Component/Outing/TimeSelectButton.swift
+++ Projects/Presentation/Sources/Scene/Apply/Component/Outing/TimeSelectButton.swift
@@ -11,6 +11,7 @@ import DesignSystem
 
 public class TimeSelectButton: BaseButton {
     public var buttonTap: ControlEvent<Void> {
+        bindAction()
         return self.rx.tap
     }
 
@@ -19,26 +20,44 @@ public class TimeSelectButton: BaseButton {
             self.attribute()
         }
     }
-    
+
     private var borderColor: UIColor {
         isSelected ? .main500 : .clear
     }
-    
+
+    private let textLabel = PiCKLabel(text: "선택", textColor: .gray500, font: .caption1)
+
+    public func setup(text: String) {
+        self.textLabel.text = text
+    }
+
     public override func attribute() {
-        self.setTitle("선택", for: .normal)
-        self.setTitleColor(.gray500, for: .normal)
-        self.setTitleColor(.gray500, for: .selected)
-        self.titleLabel?.font = .caption1
         self.layer.border(color: borderColor, width: 1)
         self.layer.cornerRadius = 8
         self.backgroundColor = .gray50
         self.tintColor = .clear
-        self.titleEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 40)
+    }
+    public override func bindAction() {
+        self.rx.tap
+            .bind(onNext: { [weak self] in
+                if self?.textLabel.text == "선택" {
+                    self?.textLabel.textColor = .gray500
+                } else {
+                    self?.textLabel.textColor = .modeBlack
+                }
+            }).disposed(by: disposeBag)
     }
     public override func layout() {
+        self.addSubview(textLabel)
+
         self.snp.makeConstraints {
             $0.width.equalTo(100)
             $0.height.equalTo(43)
+        }
+        textLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(10)
+            $0.trailing.equalToSuperview().inset(10)
         }
     }
 
