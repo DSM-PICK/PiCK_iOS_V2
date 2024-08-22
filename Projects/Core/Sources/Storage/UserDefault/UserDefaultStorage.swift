e299e1d81849e905baebee7d194d19e91e8e81ac
Choyoungjun <cyj070513@gmail.com>
2024-08-22 23:37:35 +0900

    CHORE :: [#53] 정리

--- Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
+++ Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
@@ -1,20 +1,19 @@
 import UIKit
 
 public enum UserDefaultKeys: String {
-    case homeViewMode = "viewMode"
+    case homeViewMode = "homeViewMode"
     case displayMode = "displayMode"
 }
 
 public struct UserDefaultsManager: UserDefault {
-    
     public static let shared = UserDefaultsManager()
-    
+
     public func set<T>(to: T, forKey: UserDefaultKeys) {
         UserDefaults.standard.set(to, forKey: forKey.rawValue)
     }
-    
+
     public func get(forKey: UserDefaultKeys) -> Any? {
         return UserDefaults.standard.object(forKey: forKey.rawValue)
     }
-    
+
 }
