7f71af27106d61b11ee93323c6bd3259b8ca8fb1
Choyoungjun <cyj070513@gmail.com>
2024-08-23 12:18:39 +0900

    FEAT :: [#53] UserDefault Decode & Encode 함수 추가

--- Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
+++ Projects/Core/Sources/Storage/UserDefault/UserDefaultStorage.swift
@@ -11,9 +11,33 @@ public struct UserDefaultsManager: UserDefault {
     public func set<T>(to: T, forKey: UserDefaultKeys) {
         UserDefaults.standard.set(to, forKey: forKey.rawValue)
     }
+    public func setUserDataType<T: Codable>(to: T, forKey: UserDefaultKeys) {
+        let encoder = JSONEncoder()
+
+        do {
+            let encodedData = try encoder.encode(to)
+            UserDefaults.standard.set(encodedData, forKey: forKey.rawValue)
+        } catch {
+            print("Fail To Encode UserDataType")
+        }
+    }
 
     public func get(forKey: UserDefaultKeys) -> Any? {
         return UserDefaults.standard.object(forKey: forKey.rawValue)
+    }
+
+    public func getUserDataType<T: Codable>(forKey: UserDefaultKeys, type: T.Type) -> Any? {
+        let decoder = JSONDecoder()
+
+        if let data = UserDefaults.standard.object(forKey: forKey.rawValue) {
+            do {
+                let decodedData = try decoder.decode(T.self, from: data as! Data)
+                return decodedData
+            } catch {
+                print("Fail To Decode UserDataType")
+            }
+        }
+        return nil
     }
 
 }
