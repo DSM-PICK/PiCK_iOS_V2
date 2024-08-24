26993ca9381d55497c1f33960f48161668ef70bf
Choyoungjun <cyj070513@gmail.com>
2024-08-24 22:08:08 +0900

    FEAT :: [#53] HeaderView 개발 중....

--- Projects/Presentation/Sources/Scene/Home/View/HomeTimeTableView.swift
+++ Projects/Presentation/Sources/Scene/Home/View/HomeTimeTableView.swift
@@ -5,10 +5,28 @@ import Then
 
 import RxSwift
 import RxCocoa
+import RxDataSources
 
 import Core
 import Domain
 import DesignSystem
+
+struct ddd {
+    var text = "fdjsklfjdslk"
+}
+struct TimeTableHeadrElement {
+  var header: String
+  var items: [Item]
+}
+
+extension TimeTableHeadrElement: SectionModelType {
+  typealias Item = ddd
+  
+  init(original: TimeTableHeadrElement, items: [ddd]) {
+    self = original
+    self.items = items
+  }
+}
 
 public class HomeTimeTableView: BaseView {
     private let timeTableData = BehaviorRelay<[TimeTableEntityElement]>(value: [])
@@ -19,6 +37,24 @@ public class HomeTimeTableView: BaseView {
         font: .body1,
         isHidden: true
     )
+    private lazy var datasource = RxCollectionViewSectionedReloadDataSource<TimeTableHeadrElement>(
+        configureCell: { dataSource, collectionView, indexPath, boardPost in
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: TimeTableCollectionViewCell.identifier,
+                for: indexPath
+                ) as? TimeTableCollectionViewCell
+//                  cell?.configureCell(with: boardPost)
+            return cell ?? UICollectionViewCell()
+        }, configureSupplementaryView: { dataSource, collectionview, title, indexPath in
+            let header = collectionview.dequeueReusableSupplementaryView(
+              ofKind: UICollectionView.elementKindSectionHeader,
+              withReuseIdentifier: HomeHeaderView.identifier,
+              for: indexPath
+            ) as? HomeHeaderView
+            return header ?? UICollectionReusableView()
+        }
+    )
+    private lazy var headerView = HomeHeaderView()
     private lazy var collectionViewFlowLayout = UICollectionViewFlowLayout().then {
         $0.scrollDirection = .vertical
         $0.itemSize = .init(width: self.frame.width, height: 44)
@@ -32,10 +68,15 @@ public class HomeTimeTableView: BaseView {
         $0.backgroundColor = .background
         $0.showsHorizontalScrollIndicator = false
         $0.showsVerticalScrollIndicator = false
-            $0.register(
-                TimeTableCollectionViewCell.self,
-                forCellWithReuseIdentifier: TimeTableCollectionViewCell.identifier
-            )
+        $0.register(
+            TimeTableCollectionViewCell.self,
+            forCellWithReuseIdentifier: TimeTableCollectionViewCell.identifier
+        )
+        $0.register(
+            HomeHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: HomeHeaderView.identifier
+        )
         $0.bounces = false
     }
 
@@ -45,6 +86,12 @@ public class HomeTimeTableView: BaseView {
         self.timeTableData.accept(timeTableData)
     }
 
+    var sections = [
+        TimeTableHeadrElement(
+            header: "fjdslk",
+            items: [ddd()]
+        )
+      ]
     public override func bind() {
         timeTableData.asObservable()
             .bind(to: collectionView.rx.items(
@@ -57,6 +104,9 @@ public class HomeTimeTableView: BaseView {
                 }
                 cell.adapt(model: item)
             }.disposed(by: disposeBag)
+        Observable.just(sections)
+            .bind(to: collectionView.rx.items(dataSource: datasource))
+            .disposed(by: disposeBag)
     }
     public override func layout() {
         [
