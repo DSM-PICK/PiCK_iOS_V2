76f49e527cbd2a610004a9f838be6cec35caca15
Choyoungjun <cyj070513@gmail.com>
2024-08-26 08:59:00 +0900

    FEAT :: [#56] 시간표 구현

--- Projects/Presentation/Sources/Scene/Schedule/TimeTable/View/TimeTableView.swift
+++ Projects/Presentation/Sources/Scene/Schedule/TimeTable/View/TimeTableView.swift
@@ -2,6 +2,7 @@ import UIKit
 
 import SnapKit
 import Then
+
 import RxSwift
 import RxCocoa
 
@@ -10,15 +11,15 @@ import Domain
 import DesignSystem
 
 public class TimeTableView: BaseView {
-    
-//    private lazy var timeTableData = BehaviorRelay<WeekTimeTableEntity>(value: [.init(date: "fjdksl", timetables: [.init(id: UUID(), period: 1, subjectName: "fjdksl")])])
-    
-    private var date = String()
-    
+    private lazy var timeTableData = BehaviorRelay<WeekTimeTableEntity>(value: [])
+
     private lazy var collectionViewLayout = UICollectionViewFlowLayout().then {
         $0.scrollDirection = .horizontal
         $0.minimumLineSpacing = 0
-        $0.itemSize = .init(width: self.frame.width, height: self.frame.height)
+        $0.itemSize = .init(
+            width: self.frame.width,
+            height: self.frame.height
+        )
     }
     private lazy var collectionView = UICollectionView(
         frame: .zero,
@@ -33,13 +34,11 @@ public class TimeTableView: BaseView {
             TimeTableBackgroundViewCell.self,
             forCellWithReuseIdentifier: TimeTableBackgroundViewCell.identifier
         )
-        $0.delegate = self
-        $0.dataSource = self
     }
     private lazy var pageControl = PiCKPageControl().then {
         $0.numberOfPages = 5
         $0.currentPage = 0
-        $0.backgroundColor = .white
+        $0.backgroundColor = .background
         $0.currentPageIndicatorTintColor = .main500
         $0.pageIndicatorTintColor = .gray100
         $0.allowsContinuousInteraction = false
@@ -47,31 +46,23 @@ public class TimeTableView: BaseView {
         $0.dotRadius = 4
         $0.dotSpacings = 4
     }
-    
-//    public func dateSetup(
-//        date: String
-//    ) {
-//        self.date = date
-//    }
-//    public func timeTableSetup(
-//        timeTableData: WeekTimeTableEntity
-//    ) {
-//        self.timeTableData.accept(timeTableData)
-//    }
+
+    public func timeTableSetup(
+        timeTableData: WeekTimeTableEntity
+    ) {
+        self.timeTableData.accept(timeTableData)
+    }
 
     public override func bind() {
-//        timeTableData.asObservable()
-//            .bind(to: collectionView.rx.items(
-//                cellIdentifier: TimeTableBackgroundViewCell.identifier,
-//                cellType: TimeTableBackgroundViewCell.self
-//            )) { row, element, cell in
-//                cell.setup(
-//                    date: "fjdskl",
-//                    timeTableData: element.timetables
-//                )
-//            }
-//            .disposed(by: disposeBag)
-        
+        timeTableData.asObservable()
+            .bind(to: collectionView.rx.items(
+                cellIdentifier: TimeTableBackgroundViewCell.identifier,
+                cellType: TimeTableBackgroundViewCell.self
+            )) { row, item, cell in
+                cell.adapt(model: item)
+            }
+            .disposed(by: disposeBag)
+
         collectionView.rx.didScroll
             .bind { [weak self] _ in
                 guard let collectionView = self?.collectionView else { return }
@@ -86,27 +77,12 @@ public class TimeTableView: BaseView {
         ].forEach { self.addSubview($0) }
 
         collectionView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(155)
+            $0.edges.equalToSuperview()
         }
         pageControl.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
-    }
-    
-}
-
-extension TimeTableView: UICollectionViewDelegate, UICollectionViewDataSource {
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TimeTableBackgroundViewCell.identifier, for: indexPath) as? TimeTableBackgroundViewCell else {
-            return UICollectionViewCell()
-        }
-        return cell
     }
 
 }
