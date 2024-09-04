44fe69448d10f4fd4cfe71d91a7614b8cc6415ba
Choyoungjun <cyj070513@gmail.com>
2024-09-04 13:44:48 +0900

    FEAT :: [#63] 컴포넌트

--- Projects/Presentation/Sources/Scene/Home/View/HomeHeaderView.swift
+++ Projects/Presentation/Sources/Scene/Home/View/HomeHeaderView.swift
@@ -10,37 +10,81 @@ import Core
 import Domain
 import DesignSystem
 
-public class HomeHeaderView: UICollectionReusableView {
-    public static let identifier = "HomeHeaderView"
+public enum HeaderType {
+    case waitOuting, waitClassRoom
+    case outing, classRoom
+}
 
-    private let backgroundView = UIView().then {
-        $0.backgroundColor = .gray50
+public class HomeHeaderView: BaseView {
+    public var buttonTap: ControlEvent<Void> {
+        return button.rx.tap
+    }
+
+    private let contentLabel = PiCKLabel(
+        textColor: .modeBlack,
+        font: .label1
+    )
+    private let button = UIButton(type: .system).then {
+        $0.setTitleColor(.modeWhite, for: .normal)
+        $0.titleLabel?.font = .button2
+        $0.backgroundColor = .main400
         $0.layer.cornerRadius = 8
     }
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        attribute()
-    }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-        layout()
+
+    private let waitingTitleLabel = PiCKLabel(
+        textColor: .main500,
+        font: .subTitle2
+    )
+    private let waitingContentLabel = PiCKLabel(
+        textColor: .gray500,
+        font: .caption2
+    )
+
+    public func setup(
+        type: OutingType,
+        outingText: String?,
+        classRoomText: String?,
+        waitingTitleText: String?,
+        waitingContentText: String?
+    ) {
+//        switch type {
+//        case .application:
+//            waitingTitleLabel.text = waitingTitleText
+//            waitingContentLabel.text = waitingContentText
+//        case .earlyReturn:
+//            
+//        case .classroom:
+//            <#code#>
+//        }
     }
 
-    private func attribute() {
-        self.backgroundColor = .background
+    public override func attribute() {
+        self.backgroundColor = .gray50
     }
+    public override func layout() {
+        [
+            contentLabel,
+            button,
+            waitingTitleLabel,
+            waitingContentLabel
+        ].forEach { self.addSubview($0) }
 
-    private func layout() {
-        self.addSubview(backgroundView)
-
-        backgroundView.snp.makeConstraints {
+        contentLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(24)
-            $0.height.equalTo(59)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        button.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(24)
+            $0.width.equalTo(120)
+            $0.height.equalTo(40)
         }
     }
 
+//    private func setupType(type: OutingType) {
+//        self.button.isHidden = type == .
+//        self.contentLabel.isHidden = type == .waitOuting
+//        self.waitingTitleLabel.isHidden = type == .outing
+//        self.waitingContentLabel.isHidden = type == .outing
+//    }
 }
