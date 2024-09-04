84984186582f81974e35455ddbce38b982b1823a
Choyoungjun <cyj070513@gmail.com>
2024-09-04 13:37:49 +0900

    REFACT :: [#63] 홈 로직

--- Projects/Domain/Sources/Entity/Home/MainEntity.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-import Core
-
-public struct MainEntity {
-
-}
---
+++ Projects/Domain/Sources/Repository/Home/HomeRepository.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+import RxSwift
+
+import Core
+
+public protocol HomeRepository {
+    func fetchMainData() -> Single<HomeApplyStatusEntity?>
+}
