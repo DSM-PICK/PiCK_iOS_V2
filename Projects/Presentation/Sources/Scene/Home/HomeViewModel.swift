1e59d41b84b7cc73742846ca72893880c0973c2c
Choyoungjun <cyj070513@gmail.com>
2024-09-04 13:40:34 +0900

    FEAT :: [#63] 홈 서버통신

--- Projects/Presentation/Sources/Scene/Home/HomeViewModel.swift
+++ Projects/Presentation/Sources/Scene/Home/HomeViewModel.swift
@@ -11,43 +11,54 @@ public class HomeViewModel: BaseViewModel, Stepper {
     private let disposeBag = DisposeBag()
     public var steps = PublishRelay<Step>()
 
+    private let fetchApplyStatusUseCase: FetchApplyStatusUsecase
     private let timeTableUseCase: FetchTodayTimeTableUseCase
     private let schoolMealUseCase: FetchSchoolMealUseCase
     private let noticeListUseCase: FetchNoticeListUseCase
     private let selfStudyUseCase: FetchSelfStudyUseCase
+    private let fetchOutingPassUseCase: FetchOutingPassUseCase
 
     public init(
+        fetchApplyStatusUseCase: FetchApplyStatusUsecase,
         timeTableUseCase: FetchTodayTimeTableUseCase,
         schoolMealUseCase: FetchSchoolMealUseCase,
         noticeListUseCase: FetchNoticeListUseCase,
-        selfStudyUseCase: FetchSelfStudyUseCase
+        selfStudyUseCase: FetchSelfStudyUseCase,
+        fetchOutingPassUseCase: FetchOutingPassUseCase
     ) {
+        self.fetchApplyStatusUseCase = fetchApplyStatusUseCase
         self.timeTableUseCase = timeTableUseCase
         self.schoolMealUseCase = schoolMealUseCase
         self.noticeListUseCase = noticeListUseCase
         self.selfStudyUseCase = selfStudyUseCase
+        self.fetchOutingPassUseCase = fetchOutingPassUseCase
     }
 
     public struct Input {
         let viewWillApper: Observable<Void>
         let clickAlert: Observable<Void>
+        let clickOutingPass: Observable<Void>
         let clickViewMoreNotice: Observable<Void>
         let todayDate: String
     }
     public struct Output {
         let viewMode: Signal<HomeViewType>
+        let applyStatusData: Signal<HomeApplyStatusEntity?>
         let timetableData: Driver<[TimeTableEntityElement]>
         let schoolMealData: Driver<[(Int, String, MealEntityElement)]>
         let noticeListData: Driver<NoticeListEntity>
         let selfStudyData: Driver<SelfStudyEntity>
+        let outingPassData: Signal<OutingPassEntity>
         let timeTableHeight: Driver<CGFloat>
         let schoolMealHeight: Driver<CGFloat>
         let noticeViewHeight: Driver<CGFloat>
     }
 
     private let viewModeData = PublishRelay<HomeViewType>()
+    private let applyStatusData = PublishRelay<HomeApplyStatusEntity?>()
     private let timetableData = BehaviorRelay<[TimeTableEntityElement]>(value: [])
     private let schoolMealData = BehaviorRelay<[(Int, String, MealEntityElement)]>(value: [])
+    private let outingPassData = PublishRelay<OutingPassEntity>()
     private let noticeListData = BehaviorRelay<NoticeListEntity>(value: [])
     private let selfStudyData = BehaviorRelay<SelfStudyEntity>(value: [])
     private let timeTableHeight = PublishRelay<CGFloat>()
@@ -64,6 +75,17 @@ public class HomeViewModel: BaseViewModel, Stepper {
                     self?.viewModeData.accept(.timeTable)
                 }
             }).disposed(by: disposeBag)
+
+        input.viewWillApper
+            .flatMap {
+                self.fetchApplyStatusUseCase.execute()
+                    .catch {
+                        print($0.localizedDescription)
+                        return .never()
+                    }
+            }
+            .bind(to: applyStatusData)
+            .disposed(by: disposeBag)
 
         input.viewWillApper
             .flatMap {
@@ -121,6 +143,17 @@ public class HomeViewModel: BaseViewModel, Stepper {
             .bind(to: selfStudyData)
             .disposed(by: disposeBag)
 
+        input.clickOutingPass
+            .flatMap {
+                self.fetchOutingPassUseCase.execute()
+                    .catch {
+                        print($0.localizedDescription)
+                        return .never()
+                    }
+            }
+            .bind(to: outingPassData)
+            .disposed(by: disposeBag)
+
         input.clickAlert
             .map { PiCKStep.alertIsRequired }
             .bind(to: steps)
@@ -133,10 +166,12 @@ public class HomeViewModel: BaseViewModel, Stepper {
 
         return Output(
             viewMode: viewModeData.asSignal(), 
+            applyStatusData: applyStatusData.asSignal(),
             timetableData: timetableData.asDriver(),
             schoolMealData: schoolMealData.asDriver(),
             noticeListData: noticeListData.asDriver(),
             selfStudyData: selfStudyData.asDriver(),
+            outingPassData: outingPassData.asSignal(),
             timeTableHeight: timeTableHeight.asDriver(onErrorJustReturn: 0),
             schoolMealHeight: schoolMealHeight.asDriver(onErrorJustReturn: 0),
             noticeViewHeight: noticeViewHeight.asDriver(onErrorJustReturn: 0)
