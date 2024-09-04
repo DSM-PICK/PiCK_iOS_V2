4eef19b9a96c048f63de22f0b8ec6f8dfd455f0b
Choyoungjun <cyj070513@gmail.com>
2024-09-04 23:47:10 +0900

    FEAT :: [#69] 알림 설정 뷰 퍼블리싱

---
+++ Projects/Presentation/Sources/Scene/AllTab/Notification/NotificationViewModel.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+import RxFlow
+
+import Core
+import Domain
+
+public class NotificationViewModel: BaseViewModel, Stepper {
+    private let disposeBag = DisposeBag()
+    public var steps = PublishRelay<Step>()
+
+//    private let profileUsecase: FetchDetailProfileUseCase
+//
+//    public init(profileUsecase: FetchDetailProfileUseCase) {
+//        self.profileUsecase = profileUsecase
+//    }
+    public init() { }
+    
+    public struct Input {
+//        let viewWillAppear: Observable<Void>
+    }
+    public struct Output {
+//        let profileData: Signal<DetailProfileEntity>
+    }
+
+//    private let profileData = PublishRelay<DetailProfileEntity>()
+
+    public func transform(input: Input) -> Output {
+//        input.viewWillAppear
+//            .flatMap {
+//                self.profileUsecase.execute()
+//                    .catch {
+//                        print($0.localizedDescription)
+//                        return .never()
+//                    }
+//            }
+//            .bind(to: profileData)
+//            .disposed(by: disposeBag)
+
+        return Output()
+    }
+
+}
