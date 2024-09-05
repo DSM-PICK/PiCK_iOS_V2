c0364bd89533f3a9a2ae02ed5b2c7927c77a6a92
Choyoungjun <cyj070513@gmail.com>
2024-09-05 17:28:06 +0900

    FEAT :: [#69] Tab 액션 추가

--- Projects/Presentation/Sources/Scene/AllTab/AllTabViewController.swift
+++ Projects/Presentation/Sources/Scene/AllTab/AllTabViewController.swift
@@ -12,11 +12,26 @@ import DesignSystem
 public class AllTabViewController: BaseViewController<AllTabViewModel> {
     private let logoutRelay = PublishRelay<Void>()
 
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.showsHorizontalScrollIndicator = false
+    }
+    private let contentView = UIView()
+    private let mainView = UIView()
+
     private lazy var navigationBar = PiCKMainNavigationBar(view: self)
     private let profileView = PiCKProfileView()
     private let helpSectionView = HelpSectionView()
+    private let settingSectionView = SettingSectionView()
     private let accountSectionView = AccountSectionView()
-
+//    private lazy var sectionStackVeiw = UIStackView(arrangedSubviews: [
+//        helpSectionView,
+//        settingSectionView,
+//        accountSectionView
+//    ]).then {
+//        $0.axis = .vertical
+//        $0.spacing = 24
+//    }
     public override func configureNavgationBarLayOutSubviews() {
         super.configureNavgationBarLayOutSubviews()
         
@@ -27,6 +42,8 @@ public class AllTabViewController: BaseViewController<AllTabViewModel> {
             clickNoticeTab: helpSectionView.getSelectedItem(type: .notice).asObservable(),
             clickSelfStudyTab: helpSectionView.getSelectedItem(type: .selfStudy).asObservable(),
             clickBugReportTab: helpSectionView.getSelectedItem(type: .bugReport).asObservable(),
+            clickCutomTab: settingSectionView.getSelectedItem(type: .custom),
+            clickNotificationSettingTab: settingSectionView.getSelectedItem(type: .notificationSetting),
             clickMyPageTab: accountSectionView.getSelectedItem(type: .myPage),
             clickLogOutTab: logoutRelay.asObservable()
         )
@@ -50,9 +67,17 @@ public class AllTabViewController: BaseViewController<AllTabViewModel> {
         [
             navigationBar,
             profileView,
-            helpSectionView,
-            accountSectionView
+            scrollView
         ].forEach { view.addSubview($0) }
+
+        scrollView.addSubview(contentView)
+        contentView.addSubview(mainView)
+
+        [
+            helpSectionView,
+            settingSectionView,
+            accountSectionView
+        ].forEach { mainView.addSubview($0) }
     }
     public override func setLayout() {
         navigationBar.snp.makeConstraints {
@@ -63,12 +88,31 @@ public class AllTabViewController: BaseViewController<AllTabViewModel> {
             $0.top.equalTo(navigationBar.snp.bottom).offset(24)
             $0.leading.trailing.equalToSuperview()
         }
-        helpSectionView.snp.makeConstraints {
+
+        scrollView.snp.makeConstraints {
             $0.top.equalTo(profileView.snp.bottom).offset(32)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        contentView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalTo(self.view)
+        }
+        mainView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalTo(self.view.frame.height)
+        }
+
+        helpSectionView.snp.makeConstraints {
+//            $0.top.equalTo(profileView.snp.bottom).offset(32)
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        settingSectionView.snp.makeConstraints {
+            $0.top.equalTo(helpSectionView.snp.bottom).offset(24)
             $0.leading.trailing.equalToSuperview().inset(24)
         }
         accountSectionView.snp.makeConstraints {
-            $0.top.equalTo(helpSectionView.snp.bottom).offset(24)
+            $0.top.equalTo(settingSectionView.snp.bottom).offset(24)
             $0.leading.trailing.equalToSuperview().inset(24)
         }
     }
