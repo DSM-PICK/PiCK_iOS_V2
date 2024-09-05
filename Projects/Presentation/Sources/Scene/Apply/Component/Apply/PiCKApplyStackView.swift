7a85a10450e5d3aefd163680eaf68c7212c03829
Choyoungjun <cyj070513@gmail.com>
2024-09-05 17:27:09 +0900

    CHORE :: [#69] 자잘한 수정

--- Projects/Presentation/Sources/Scene/Apply/Component/Apply/PiCKApplyStackView.swift
+++ Projects/Presentation/Sources/Scene/Apply/Component/Apply/PiCKApplyStackView.swift
@@ -12,34 +12,34 @@ import DesignSystem
 
 public class PiCKApplyStackView: BaseView {
     public var clickWeekendMealTab: ControlEvent<Void> {
-        return weekendMealApplyTab.clickApplyButton
+        return weekendMealApplyTab.clickActionButton
     }
     public var clickClassRoomMoveTab: ControlEvent<Void> {
-        return classroomMoveApplyTab.clickApplyButton
+        return classroomMoveApplyTab.clickActionButton
     }
     public var clickOutingTab: ControlEvent<Void> {
-        return outingApplyTab.clickApplyButton
+        return outingApplyTab.clickActionButton
     }
     public var clickEarlyLeaveTab: ControlEvent<Void> {
-        return earlyLeaveApplyTab.clickApplyButton
+        return earlyLeaveApplyTab.clickActionButton
     }
 
-    private let weekendMealApplyTab = PiCKApplyView(
+    private let weekendMealApplyTab = PiCKTabView(
         title: "주말 급식 신청",
         explain: "지금은 주말급식 신청 기간입니다.\n주말 급식 신청은 매달 한 번 한정된 기간에 합니다.",
         icon: .weekendMeal
     )
-    private let classroomMoveApplyTab = PiCKApplyView(
+    private let classroomMoveApplyTab = PiCKTabView(
         title: "교실 이동 신청",
         explain: "선생님께서 수락하시기 전엔 이동할 수 없습니다.\n수락 후 이동하시기 바랍니다.",
         icon: .classRoomMove
     )
-    private let outingApplyTab = PiCKApplyView(
+    private let outingApplyTab = PiCKTabView(
         title: "외출 신청",
         explain: "선생님께 미리 수락을 받은 뒤 신청합니다.",
         icon: .outing
     )
-    private let earlyLeaveApplyTab = PiCKApplyView(
+    private let earlyLeaveApplyTab = PiCKTabView(
         title: "조기 귀가 신청",
         explain: "선생님께 미리 수락을 받은 뒤 신청합니다.",
         icon: .earlyLeave
@@ -88,7 +88,7 @@ public class PiCKApplyStackView: BaseView {
         }
     }
 
-    private func toggleTabAction(selectedTab: PiCKApplyView) {
+    private func toggleTabAction(selectedTab: PiCKTabView) {
         let array = [
             weekendMealApplyTab,
             classroomMoveApplyTab,
