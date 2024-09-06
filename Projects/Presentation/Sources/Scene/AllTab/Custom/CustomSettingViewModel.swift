b82498ceaf469aa18eac0849c69681490f485f58
Choyoungjun <cyj070513@gmail.com>
2024-09-06 15:15:52 +0900

    FEAT :: [#69] CustomSetting 기능 구현

--- Projects/Presentation/Sources/Scene/AllTab/Custom/CustomSettingViewModel.swift
+++ Projects/Presentation/Sources/Scene/AllTab/Custom/CustomSettingViewModel.swift
@@ -13,13 +13,32 @@ public class CustomSettingViewModel: BaseViewModel, Stepper {
 
     private let userDefaultStorage = UserDefaultsManager.shared
     private var homeViewType: HomeViewType {
-        let value = userDefaultStorage.getUserDataType(forKey: .homeViewMode, type: HomeViewType.self) as? HomeViewType
+        let value = userDefaultStorage.getUserDataType(
+            forKey: .homeViewMode,
+            type: HomeViewType.self
+        ) as? HomeViewType
 
         value == .timeTable ?
-        tabViewText.accept(("메인페이지에서 급식 또는 시간표를 볼 수 있어요!\n현재는 시간표로 설정되어 있어요.", "급식으로 보기")) :
-        tabViewText.accept(("메인페이지에서 급식 또는 시간표를 볼 수 있어요!\n현재는 급식으로 설정되어 있어요.", "시간표로 보기"))
+        homeSettingTabViewText.accept(("메인페이지에서 급식 또는 시간표를 볼 수 있어요!\n현재는 시간표로 설정되어 있어요.", "급식으로 보기")) :
+        homeSettingTabViewText.accept(("메인페이지에서 급식 또는 시간표를 볼 수 있어요!\n현재는 급식으로 설정되어 있어요.", "시간표로 보기"))
 
         return value == .timeTable ? .schoolMeal : .timeTable
+    }
+
+    private var pickerTimeType: PickerTimeSelectType {
+        var value = userDefaultStorage.getUserDataType(
+            forKey: .pickerTimeMode,
+            type: PickerTimeSelectType.self
+        ) as? PickerTimeSelectType
+
+        if value == .none {
+            value = .time
+        }
+        value == .time ?
+        pickerSettingTabViewText.accept(("픽에서 신청할 때 시간 또는 교시로 설정할 수 있어요!\n현재는 시간으로 설정되어 있어요.", "교시로 설정하기")) :
+        pickerSettingTabViewText.accept(("픽에서 신청할 때 시간 또는 교시로 설정할 수 있어요!\n현재는 교시로 설정되어 있어요.", "시간으로 설정하기"))
+
+        return value == .time ? .period : .time
     }
 
     public init() { }
@@ -27,18 +46,21 @@ public class CustomSettingViewModel: BaseViewModel, Stepper {
     public struct Input {
         let viewWillAppear: Observable<Void>
         let clickHomeSetting: Observable<Void>
-        let clickApplyAlertSetting: Observable<Void>
+        let clickPickerSetting: Observable<Void>
     }
     public struct Output { 
-        let tabViewText: Driver<(String, String)>
+        let homeSettingTabViewText: Driver<(String, String)>
+        let pickerSettingTabViewText: Driver<(String, String)>
     }
 
-    private let tabViewText = BehaviorRelay<(String, String)>(value: ("", ""))
+    private let homeSettingTabViewText = BehaviorRelay<(String, String)>(value: ("", ""))
+    private let pickerSettingTabViewText = BehaviorRelay<(String, String)>(value: ("", ""))
 
     public func transform(input: Input) -> Output {
         input.viewWillAppear
             .subscribe(onNext: {
                 _ = self.homeViewType
+                _ = self.pickerTimeType
             }).disposed(by: disposeBag)
 
         input.clickHomeSetting
@@ -62,7 +84,31 @@ public class CustomSettingViewModel: BaseViewModel, Stepper {
                 )
             }).disposed(by: disposeBag)
 
-        return Output(tabViewText: tabViewText.asDriver())
+        input.clickPickerSetting
+            .subscribe(onNext: {
+                self.steps.accept(
+                    PiCKStep.applyAlertIsRequired(
+                        successType: .success,
+                        alertType: .complete
+                    )
+                )
+                self.userDefaultStorage.setUserDataType(
+                    to: self.pickerTimeType,
+                    forKey: .pickerTimeMode
+                )
+            }, onError: {_ in
+                self.steps.accept(
+                    PiCKStep.applyAlertIsRequired(
+                        successType: .fail,
+                        alertType: .complete
+                    )
+                )
+            }).disposed(by: disposeBag)
+
+        return Output(
+            homeSettingTabViewText: homeSettingTabViewText.asDriver(),
+            pickerSettingTabViewText: pickerSettingTabViewText.asDriver()
+        )
     }
 
 }
