e72949c2d1524374a45a1c7be31835f2ea75cf58
Choyoungjun <cyj070513@gmail.com>
2024-09-11 09:38:00 +0900

    FEAT :: [#76] 알림 설정 로직

---
+++ Projects/Data/Sources/DTO/Notification/NotificationDTO.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+import Core
+
+import Domain
+
+public struct NotificationDTO: Decodable {
+    let subscribeTopicResponse: [NotificationDTOElement]
+
+    enum CodingKeys: String, CodingKey {
+        case subscribeTopicResponse = "subscribe_topic_response"
+    }
+}
+
+extension NotificationDTO {
+    func toDomain() -> NotificationEntity {
+        return .init(
+            subscribeTopicResponse: subscribeTopicResponse.map { $0.toDomain() }
+        )
+    }
+
+}
+
+public struct NotificationDTOElement: Decodable {
+    let topic: NotificationType.RawValue
+    let isSubscribed: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case topic
+        case isSubscribed = "is_subscribed"
+    }
+}
+
+extension NotificationDTOElement {
+    func toDomain() -> NotificationEntityElement {
+        return .init(
+            topic: topic, 
+            isSubscribed: isSubscribed
+        )
+    }
+
+}
