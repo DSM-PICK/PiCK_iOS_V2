ee39a9ba6a8b8a669f2356f60160e1d72010f086
Choyoungjun <cyj070513@gmail.com>
2024-09-13 12:59:58 +0900

    CHORE :: [73] 줄 정리

--- Projects/Core/Sources/Storage/KeyChain/KeychainImpl.swift
+++ Projects/Core/Sources/Storage/KeyChain/KeychainImpl.swift
@@ -2,19 +2,19 @@ import KeychainSwift
 
 public struct KeychainImpl: Keychain {
     private let keychain = KeychainSwift()
-    
+
     public init() {}
-    
+
     public func save(type: KeychainType, value: String) {
         keychain.set(value, forKey: type.rawValue)
     }
-    
+
     public func load(type: KeychainType) -> String {
         return keychain.get(type.rawValue) ?? "Failed To Load Keychain Value"
     }
-    
+
     public func delete(type: KeychainType) {
         keychain.delete(type.rawValue)
     }
-    
+
 }
