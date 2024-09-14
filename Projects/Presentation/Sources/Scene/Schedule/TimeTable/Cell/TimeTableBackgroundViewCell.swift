c38009b734e6bf5d2ba2804678b5f0e6cb33a801
Choyoungjun <cyj070513@gmail.com>
2024-09-14 16:48:30 +0900

    FEAT :: [#84] 시간표 예외처리

--- Projects/Presentation/Sources/Scene/Schedule/TimeTable/Cell/TimeTableBackgroundViewCell.swift
+++ Projects/Presentation/Sources/Scene/Schedule/TimeTable/Cell/TimeTableBackgroundViewCell.swift
@@ -15,6 +15,11 @@ public class TimeTableBackgroundViewCell: BaseCollectionViewCell<TimeTableEntity
     private lazy var timeTableData = PublishRelay<[TimeTableEntityElement]>()
 
     private let dateLabel = PiCKLabel(textColor: .gray900, font: .label1)
+    private let emptyTimeTableLabel = PiCKLabel(
+        text: "시간표가 없습니다.",
+        textColor: .gray800,
+        font: .caption1
+    )
     private lazy var collectionviewLayout = UICollectionViewFlowLayout().then {
         $0.scrollDirection = .vertical
         $0.minimumLineSpacing = 12
@@ -36,6 +41,10 @@ public class TimeTableBackgroundViewCell: BaseCollectionViewCell<TimeTableEntity
 
         self.dateLabel.text = "\(model.date.toDate(type: .fullDate).toString(type: .dayKor))"
         self.timeTableData.accept(model.timetables)
+
+        let timeTableIsEmpty = model.timetables.isEmpty
+        self.emptyTimeTableLabel.isHidden = !timeTableIsEmpty
+        self.collectionView.isHidden = timeTableIsEmpty
     }
 
     public override func bind() {
@@ -50,12 +59,16 @@ public class TimeTableBackgroundViewCell: BaseCollectionViewCell<TimeTableEntity
     public override func layout() {
         [
             dateLabel,
+            emptyTimeTableLabel,
             collectionView
         ].forEach { self.addSubview($0) }
 
         dateLabel.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview().inset(24)
+        }
+        emptyTimeTableLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
         }
         collectionView.snp.makeConstraints {
             $0.top.equalTo(dateLabel.snp.bottom).offset(15)
