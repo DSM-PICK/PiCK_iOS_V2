81e7c039ebc1e13ff28c5714a817e8e622521792
Choyoungjun <cyj070513@gmail.com>
2024-09-24 19:33:07 +0900

    FEAT :: CI 스크립트 작성

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd ../
+git clone https://github.com/DSM-PICK/PiCK_iOS_V2_XCConfig.git
+mv PiCK_iOS_V2_XCConfig/XCConfig/ .
+
+git clone https://github.com/DSM-PICK/PiCK-iOS-GoogleInfo.git
+mv PiCK-iOS-GoogleInfo/FireBase/Projects/App/Resources/
+
+brew install make
+
+curl https://mise.jdx.dev/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+eval "$(mise activate bash --shims)"
+
+mise install tuist@3.40.0
+
+tuist version
+
+make reset
+
+tuist fetch
+TUIST_CI=1 tuist generate
