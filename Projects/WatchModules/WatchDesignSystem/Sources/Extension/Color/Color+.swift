4139a13a6208c59412f5eefbfbbece7335cf5c82
Choyoungjun <cyj070513@gmail.com>
2024-12-04 08:40:47 +0900

    feat :: [#126] 디자인 시스템 세팅

---
+++ Projects/WatchModules/WatchDesignSystem/Sources/Extension/Color/Color+.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+public extension Color {
+    // MARK: background
+    static let background = WatchDesignSystemAsset.Color.background.swiftUIColor
+    // MARK: error
+    static let error = WatchDesignSystemAsset.Color.error.swiftUIColor
+    // MARK: gray
+    static let gray50 = WatchDesignSystemAsset.Color.gray50.swiftUIColor
+    static let gray100 = WatchDesignSystemAsset.Color.gray100.swiftUIColor
+    static let gray200 = WatchDesignSystemAsset.Color.gray200.swiftUIColor
+    static let gray300 = WatchDesignSystemAsset.Color.gray300.swiftUIColor
+    static let gray400 = WatchDesignSystemAsset.Color.gray400.swiftUIColor
+    static let gray500 = WatchDesignSystemAsset.Color.gray500.swiftUIColor
+    static let gray600 = WatchDesignSystemAsset.Color.gray600.swiftUIColor
+    static let gray700 = WatchDesignSystemAsset.Color.gray700.swiftUIColor
+    static let gray800 = WatchDesignSystemAsset.Color.gray800.swiftUIColor
+    static let gray900 = WatchDesignSystemAsset.Color.gray900.swiftUIColor
+    // MARK: main
+    static let main50 = WatchDesignSystemAsset.Color.main50.swiftUIColor
+    static let main100 = WatchDesignSystemAsset.Color.main100.swiftUIColor
+    static let main200 = WatchDesignSystemAsset.Color.main200.swiftUIColor
+    static let main300 = WatchDesignSystemAsset.Color.main300.swiftUIColor
+    static let main400 = WatchDesignSystemAsset.Color.main400.swiftUIColor
+    static let main500 = WatchDesignSystemAsset.Color.main500.swiftUIColor
+    static let main600 = WatchDesignSystemAsset.Color.main600.swiftUIColor
+    static let main700 = WatchDesignSystemAsset.Color.main700.swiftUIColor
+    static let main800 = WatchDesignSystemAsset.Color.main800.swiftUIColor
+    static let main900 = WatchDesignSystemAsset.Color.main900.swiftUIColor
+    // MARK: mode
+    static let modeWhite = WatchDesignSystemAsset.Color.modeWhite.swiftUIColor
+    static let modeBlack = WatchDesignSystemAsset.Color.modeBlack.swiftUIColor
+}
