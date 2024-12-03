e71fae2f1c097d09e4c3bac16430195e9ebf7116
Choyoungjun <cyj070513@gmail.com>
2024-12-04 08:42:54 +0900

    feat :: [#126] 워치 lib 모듈 추가

---
+++ Projects/WatchModules/WatchThirdPartyLib/Project.swift
@@ -0,0 +1,17 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: "WatchThirdPartyLib",
+    platform: .watchOS,
+    product: .staticFramework,
+    deploymentTarget: .watchOS(targetVersion: "9.0"),
+    dependencies: [
+        .SPM.RxSwift,
+        .SPM.RxCocoa,
+        .SPM.Kingfisher,
+        .SPM.Moya,
+        .SPM.RxMoya
+    ]
+)
