d5ea13ad9027f3548465852ff00b355aae524d3c
Choyoungjun <cyj070513@gmail.com>
2025-04-24 08:50:14 +0900

    refact :: [#59] Plugin 리펙토링

--- Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -4,8 +4,9 @@ public struct ProjectEnvironment {
     public let appName: String
     public let targetName: String
     public let organizationName: String
-    public let deploymentTarget: DeploymentTarget
-    public let platform: Platform
+    public let deploymentTargets: DeploymentTargets
+    public let destination: Destinations
+    public let watchDestination: Destinations
     public let baseSetting: SettingsDictionary
 }
 
@@ -13,7 +14,8 @@ public let env = ProjectEnvironment(
     appName: "PiCK-iOS-V2",
     targetName: "PiCK-iOS-V2",
     organizationName: "com.pick.app",
-    deploymentTarget: .iOS(targetVersion: "16.0", devices: [.iphone, .ipad]),
-    platform: .iOS,
+    deploymentTargets: .iOS("16.0"),
+    destination: .iOS,
+    watchDestination: .watchOS,
     baseSetting: ["OTHER_LDFLAGS": ["$(inherited) -Objc"]]
 )
