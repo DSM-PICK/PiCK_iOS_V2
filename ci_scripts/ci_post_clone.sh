e11c679f6db73297364f85ea075d73b1572ad3f5
Choyoungjun <cyj070513@gmail.com>
2025-04-25 14:46:41 +0900

    chore :: ci script 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,12 +12,12 @@ curl https://mise.jdx.dev/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
 eval "$(mise activate bash --shims)"
 
-mise install tuist@3.40.0
+mise install tuist@4.31.0
 
 tuist version
 
 make clean
 make cache_clean
 
-tuist fetch
+tuist install
 TUIST_CI=1 tuist generate
