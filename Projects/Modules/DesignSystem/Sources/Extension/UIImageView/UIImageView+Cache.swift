554b69c94c9a8e1b52904ed35338b97c37ec3d9b
Choyoungjun <cyj070513@gmail.com>
2025-04-29 23:29:46 +0900

    feat :: [#166] UIImageView+Cache Extension 추가

---
+++ Projects/Modules/DesignSystem/Sources/Extension/UIImageView/UIImageView+Cache.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+import Kingfisher
+
+public extension UIImageView {
+    func setImage(with urlString: String) {
+        ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
+            switch result {
+            case .success(let value):
+                if let image = value.image {
+                    self.image = image
+                } else {
+                    guard let url = URL(string: urlString) else { return }
+                    let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
+                    self.kf.setImage(with: resource)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+}
