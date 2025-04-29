d4c327524091440885250a37ede8f11827e622aa
Choyoungjun <cyj070513@gmail.com>
2025-04-29 23:38:08 +0900

    feat :: [#166] placeholder 추가

--- Projects/Modules/DesignSystem/Sources/Extension/UIImageView/UIImageView+Cache.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/UIImageView/UIImageView+Cache.swift
@@ -3,7 +3,10 @@ import UIKit
 import Kingfisher
 
 public extension UIImageView {
-    func setImage(with urlString: String) {
+    func setImage(with urlString: String, placeholder: UIImage? = nil) {
+        if let placeholder = placeholder {
+            self.image = placeholder
+        }
         ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
             switch result {
             case .success(let value):
@@ -12,7 +15,7 @@ public extension UIImageView {
                 } else {
                     guard let url = URL(string: urlString) else { return }
                     let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
-                    self.kf.setImage(with: resource)
+                    self.kf.setImage(with: resource, placeholder: placeholder)
                 }
             case .failure(let error):
                 print(error)
