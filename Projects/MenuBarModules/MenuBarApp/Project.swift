ddf9fe2ac8b31a6d3e3edc33490470c82c1c3ddb
jihoon <leejh080628@naver.com>
2025-11-17 10:15:43 +0900

    feat :: menubarApp scheme 추가

---
+++ Projects/MenuBarModules/MenuBarApp/Project.swift
@@ -0,0 +1,66 @@
+import Foundation
+import ProjectDescription
+import ProjectDescriptionHelpers
+import EnvironmentPlugin
+import DependencyPlugin
+import ConfigurationPlugin
+
+let configurations: [Configuration] = [
+    .debug(name: .stage, xcconfig: .relativeToXCConfig(type: .stage, name: "MenuBarApp")),
+    .release(name: .prod, xcconfig: .relativeToXCConfig(type: .prod, name: "MenuBarApp"))
+]
+
+let settings: Settings = .settings(
+    base: env.baseSetting,
+    configurations: configurations,
+    defaultSettings: .recommended
+)
+
+let targets: [Target] = [
+    .target(
+        name: "MenuBarApp",
+        destinations: .macOS,
+        product: .app,
+        bundleId: "com.pick.MenuBarApp",
+        deploymentTargets: .macOS("13.0"),
+        infoPlist: .file(path: "Support/Info.plist"),
+        sources: ["Sources/**"],
+        resources: nil,
+        entitlements: "Support/MenuBarApp.entitlements",
+        dependencies: [
+            .MenuBarModule.menuBarDesignSystem,
+            .MenuBarModule.menuBarNetwork,
+            .MenuBarModule.menuBarThirdPartyLib
+        ],
+        settings: .settings(base: env.baseSetting)
+    )
+]
+
+let schemes: [Scheme] = [
+    .scheme(
+        name: "MenuBarApp-STAGE",
+        shared: true,
+        buildAction: .buildAction(targets: ["MenuBarApp"]),
+        runAction: .runAction(configuration: .stage),
+        archiveAction: .archiveAction(configuration: .stage),
+        profileAction: .profileAction(configuration: .stage),
+        analyzeAction: .analyzeAction(configuration: .stage)
+    ),
+    .scheme(
+        name: "MenuBarApp-PROD",
+        shared: true,
+        buildAction: .buildAction(targets: ["MenuBarApp"]),
+        runAction: .runAction(configuration: .prod),
+        archiveAction: .archiveAction(configuration: .prod),
+        profileAction: .profileAction(configuration: .prod),
+        analyzeAction: .analyzeAction(configuration: .prod)
+    )
+]
+
+let project = Project(
+    name: "MenuBarApp",
+    organizationName: env.organizationName,
+    settings: settings,
+    targets: targets,
+    schemes: schemes
+)
--- Workspace.swift
+++ Workspace.swift
@@ -5,6 +5,7 @@ let workspace = Workspace(
     name: env.appName,
     projects: [
         "Projects/App",
+        "Projects/MenuBarModules/MenuBarApp",
         "Projects/MenuBarModules/MenuBarNetwork",
         "Projects/MenuBarModules/MenuBarDesignSystem",
         "Projects/MenuBarModules/MenuBarThirdPartyLib"
