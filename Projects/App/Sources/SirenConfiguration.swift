4dbda2e11548c20078c8d4e34d0317b4b8161a02
Juhui <joohee090331@gmail.com>
2025-12-20 20:05:00 +0900

    chore :: 주석 삭제

--- Projects/App/Sources/SirenConfiguration.swift
+++ Projects/App/Sources/SirenConfiguration.swift
@@ -4,10 +4,9 @@ public class SirenConfiguration {
     public static func setup() {
         let siren = Siren.shared
         
-        siren.apiManager = APIManager(country: .korea) // 기준 위치 대한민국 앱스토어
+        siren.apiManager = APIManager(country: .korea)
         
         Siren.shared.presentationManager = PresentationManager (
-//            appName: "appname",
             alertTitle: "PiCK 업데이트 알림",
             alertMessage: "원활한 서비스 이용을 위해 \n최신 버전으로 업데이트해 주세요.",
             updateButtonTitle: "업데이트",
