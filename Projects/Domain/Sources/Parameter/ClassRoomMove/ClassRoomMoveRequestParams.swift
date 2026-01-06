1dee35a4fa38fefe300f6d1f136b171afa307a3c
circle0802 <hawon9781@gmail.com>
2026-01-06 19:10:00 +0900

    feat :: [#232] class-room/move에 move ""값 보내기

--- Projects/Domain/Sources/Parameter/ClassRoomMove/ClassRoomMoveRequestParams.swift
+++ Projects/Domain/Sources/Parameter/ClassRoomMove/ClassRoomMoveRequestParams.swift
@@ -1,17 +1,20 @@
 import Foundation
 
 public struct ClassroomMoveRequestParams: Encodable {
+    public let move: String
     public let floor: Int
     public let classroomName: String
     public let startPeriod: Int
     public let endPeriod: Int
 
     public init(
+        move: String,
         floor: Int,
         classroomName: String,
         startPeriod: Int,
         endPeriod: Int
     ) {
+        self.move = move
         self.floor = floor
         self.classroomName = classroomName
         self.startPeriod = startPeriod
@@ -19,6 +22,7 @@ public struct ClassroomMoveRequestParams: Encodable {
     }
 
     enum CodingKeys: String, CodingKey {
+        case move
         case floor
         case classroomName = "classroom_name"
         case startPeriod = "start"
