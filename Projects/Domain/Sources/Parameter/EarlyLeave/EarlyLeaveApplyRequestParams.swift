cdaa6a323b6e05f17859893e3cf6643265f3073a
circle0802 <hawon9781@gmail.com>
2026-01-21 19:02:48 +0900

    feat :: [#215] Params 변경 및 화면에 적용

--- Projects/Domain/Sources/Parameter/EarlyLeave/EarlyLeaveApplyRequestParams.swift
+++ Projects/Domain/Sources/Parameter/EarlyLeave/EarlyLeaveApplyRequestParams.swift
@@ -3,18 +3,22 @@ import Foundation
 public struct EarlyLeaveApplyRequestParams: Encodable {
     public let reason: String
     public let startTime: String
+    public let applicationType: String
 
     public init(
         reason: String,
-        startTime: String
+        startTime: String,
+        applicationType: String
     ) {
         self.reason = reason
         self.startTime = startTime
+        self.applicationType = applicationType
     }
 
     enum CodingKeys: String, CodingKey {
         case reason
         case startTime = "start"
+        case applicationType = "application_type"
     }
 
 }
